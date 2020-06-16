43544d1b350ddc1065323fbb6b981f66c14e6f77
joshwlambert <j.w.l.lambert@rug.nl>
2020-06-16 21:32:41 +0100

    updated performance_analysis script

--- R/performance_analysis.R
+++ R/performance_analysis.R
@@ -1,3 +1,7 @@
+library(DAISIE)
+
+set.seed(1)
+
 lac <- c(1.0, 2.0)
 mu <- c(1.0, 2.0)
 K <- c(10, 30, Inf)
@@ -7,36 +11,153 @@ laa <- c(0.5, 1.0)
 param_space <- expand.grid(lac, mu, K, gam, laa)
 
 cr_data_set <- list()
-rr_data_set <- list()
+rr_clado_data_set_sd1 <- list()
+rr_clado_data_set_sd5 <- list()
+rr_clado_data_set_sd10 <- list()
+rr_ext_data_set_sd1 <- list()
+rr_ext_data_set_sd5 <- list()
+rr_ext_data_set_sd10 <- list()
+rr_carr_cap_data_set_sd1 <- list()
+rr_carr_cap_data_set_sd5 <- list()
+rr_carr_cap_data_set_sd10 <- list()
+rr_ana_data_set_sd1 <- list()
+rr_ana_data_set_sd5 <- list()
+rr_ana_data_set_sd10 <- list()
+
 for (i in seq_len(nrow(param_space))) {
+
   cr_data_set[[i]] <- DAISIE_sim_constant_rate(
     time = 5,
     M = 1000,
     pars = as.numeric(param_space[i, ]),
-    replicates = 100)
-  rr_data_set[[i]] <- DAISIE_sim_relaxed_rate(
+    replicates = 1) #1000
+
+  rr_clado_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
     pars = as.numeric(param_space[i, ]),
-    replicates = 100,
+    replicates = 1, #1000
     relaxed_par = "cladogenesis",
-    relaxed_rate_pars = create_relaxed_rate_pars(mean = 1, sd = 5))
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 1]),
+      sd = 1))
+
+  rr_clado_data_set_sd5[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "cladogenesis",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 1]),
+      sd = 5))
+
+  rr_clado_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "cladogenesis",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 1]),
+      sd = 10))
+
+  rr_ext_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "extinction",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 2]),
+      sd = 1))
+
+  rr_ext_data_set_sd5[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "extinction",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 2]),
+      sd = 5))
+
+  rr_ext_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "extinction",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 2]),
+      sd = 10))
+
+  rr_carr_cap_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "carrying_capacity",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 3]),
+      sd = 1))
+
+  rr_carr_cap_data_set_sd5[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "carrying_capacity",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 3]),
+      sd = 5))
+
+  rr_carr_cap_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "carrying_capacity",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 3]),
+      sd = 10))
+
+  rr_ana_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "anagenesis",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 5]),
+      sd = 1))
+
+  rr_ana_data_set_sd5[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "anagenesis",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 5]),
+      sd = 5))
+
+  rr_ana_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[i, ]),
+    replicates = 1, #1000
+    relaxed_par = "anagenesis",
+    relaxed_rate_pars = create_relaxed_rate_pars(
+      mean = as.numeric(param_space[i, 5]),
+      sd = 10))
 }
 
-subplex_results <- list()
-simplex_results <- list()
 for (i in seq_len(length(data_set))) {
-  for (j in seq_len(length(data_set[[i]])))
-    subplex_results[[i]] <- DAISIE_ML_CS(datalist = data_set[[1]][[i]][[j]],
+    results[[i]] <- DAISIE_ML_CS(datalist = data_set[[1]][[i]][[j]],
                                          initparsopt = c(0.5, 0.5, 20, 0.005, 0.5),
                                          idparsopt = 1:5,
                                          idparsfix = NULL,
                                          parsfix = NULL,
-                                         optimmethod = "subplex")
-  simplex_results[[i]] <- DAISIE_ML_CS(datalist = data_set[[1]][[i]][[j]],
-                                       initparsopt = c(0.5, 0.5, 20, 0.005, 0.5),
-                                       idparsopt = 1:5,
-                                       idparsfix = NULL,
-                                       parsfix = NULL,
-                                       optimmethod = "simplex")
+                                         optimmethod = "simplex")
 }
