e3e37f2e9d4306c400c3d22ad617c332bc9bb235
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-16 21:48:44 +0200

    added cr_sim_data and documentation

---
+++ R/cr_sim_data.R
@@ -0,0 +1,7 @@
+#' Data simulated under the constant rate model
+#'
+#' A dataset containing simulated data for 48 parameter sets and 1000 replicates
+#' per parameter set.
+#'
+#' @format An object of class \code{list}.
+"cr_sim_data"
