beeab3f755d9f3f2b0636a2b58fb598857a75be6
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-16 21:49:00 +0200

    edited generate_sim_data script

--- scripts/generate_sim_data.R
+++ scripts/generate_sim_data.R
@@ -1,26 +1,42 @@
 ### Generate simulation data for model performance analysis ###
 
-#Simulate with constant rate model
+library(DAISIE)
 
-data(param_space)
+clado <- c(1.0, 2.0)
+ext <- c(1.0, 2.0)
+carr_cap <- c(10, 30, Inf)
+immig <- c(0.005, 0.01)
+ana <- c(0.5, 1.0)
+sd <- c(1, 10)
+
+param_space <- expand.grid(clado = clado,
+                           ext = ext,
+                           carr_cap = carr_cap,
+                           immig = immig,
+                           ana = ana,
+                           sd = sd)
+
+
+#Simulate with constant rate model
 
 set.seed(1)
 
 cr_sim_data <- list()
 
-for (i in seq_len(nrow(param_space))) {
+#Only need to run the parameter sets without changes to sd
+param_sets <- nrow(param_space) / 2
+
+for (i in 1:param_sets) {
   cr_sim_data[[i]] <- DAISIE_sim_constant_rate(
     time = 5,
     M = 1000,
     pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000)
+    replicates = 10)
 }
 
 save(cr_sim_data, file = "cr_sim_data.RData")
 
 #Simulate with relaxed-rate models
-
-data(param_space)
 
 set.seed(1)
 
@@ -34,83 +50,68 @@ rr_ana_sim_data_sd1 <- list()
 rr_ana_sim_data_sd10 <- list()
 
 for (i in seq_len(nrow(param_space))) {
-  rr_clado_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_clado_sim_data_sd1[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "cladogenesis",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 1]),
-      sd = 1))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "cladogenesis")
 
-  rr_clado_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_clado_sim_data_sd10[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "cladogenesis",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 1]),
-      sd = 10))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "cladogenesis")
 
-  rr_ext_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_ext_sim_data_sd1[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "extinction",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 2]),
-      sd = 1))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "extinction")
 
-  rr_ext_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_ext_sim_data_sd10[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "extinction",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 2]),
-      sd = 10))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "extinction")
 
-  rr_carr_cap_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_carr_cap_sim_data_sd1[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "carrying_capacity",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 3]),
-      sd = 1))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "carrying_capacity")
 
-  rr_carr_cap_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_carr_cap_sim_data_sd10[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "carrying_capacity",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 3]),
-      sd = 10))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "carrying_capacity")
 
-  rr_ana_data_set_sd1[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_ana_sim_data_sd1[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "anagenesis",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 5]),
-      sd = 1))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "anagenesis")
 
-  rr_ana_data_set_sd10[[i]] <- DAISIE_sim_relaxed_rate(
+  rr_ana_sim_data_sd10[[i]] <- DAISIE_sim_relaxed_rate(
     time = 5,
     M = 1000,
-    pars = as.numeric(param_space[i, 1:5]),
-    replicates = 1000,
-    relaxed_par = "anagenesis",
-    relaxed_rate_pars = create_relaxed_rate_pars(
-      mean = as.numeric(param_space[i, 5]),
-      sd = 10))
+    pars = as.numeric(param_space[i, ]),
+    replicates = 10,
+    relaxed_par = "anagenesis")
 }
+
+save(rr_clado_sim_data_sd1, file = "rr_clado_sim_data_sd1.RData")
+save(rr_clado_sim_data_sd10, file = "rr_clado_sim_data_sd10.RData")
+save(rr_ext_sim_data_sd1, file = "rr_ext_sim_data_sd1.RData")
+save(rr_ext_sim_data_sd10, file = "rr_ext_sim_data_sd10.RData")
+save(rr_carr_cap_sim_data_sd1, file = "rr_carr_cap_sim_data_sd1.RData")
+save(rr_carr_cap_sim_data_sd10, file = "rr_carr_cap_sim_data_sd10.RData")
+save(rr_ana_sim_data_sd1, file = "rr_ana_sim_data_sd1.RData")
+save(rr_ana_sim_data_sd10, file = "rr_ana_sim_data_sd10.RData")
