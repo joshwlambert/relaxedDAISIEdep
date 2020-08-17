ab9fa230d6c964a908f57a8bab7d3ad5ca86eb9a
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-17 14:32:52 +0200

    edited rr_approx_cr script

--- scripts/rr_approx_cr.R
+++ scripts/rr_approx_cr.R
@@ -1,50 +1,46 @@
 library(DAISIE)
-load("~/github/DAISIE/data/Galapagos_datalist.RData")
+data(Galapagos_datalist)
 
-CR <- DAISIE_ML_CS(datalist = Galapagos_datalist,
-                   initparsopt = c(2.5, 2.7, 20, 0.009, 1.01),
-                   idparsopt = 1:5,
-                   parsfix = NULL,
-                   idparsfix = NULL,
-                   ddmodel = 11,
-                   CS_version = 1)
+cr <- DAISIE_ML_CS(
+  datalist = Galapagos_datalist,
+  initparsopt = c(2.5, 2.7, 20, 0.009, 1.01),
+  idparsopt = 1:5,
+  parsfix = NULL,
+  idparsfix = NULL,
+  ddmodel = 11,
+  optimmethod = "simplex",
+  CS_version = 1)
 
-relaxed_clado_sd_1 <- DAISIE_ML_CS(datalist = Galapagos_datalist,
-                                   initparsopt = c(2, 2.7, 20, 0.009, 1.01),
-                                   idparsopt = 1:5,
-                                   parsfix = NULL,
-                                   idparsfix = NULL,
-                                   ddmodel = 11,
-                                   CS_version = -2.1)
+rr_clado_sd_1 <- DAISIE_ML_CS(
+  datalist = Galapagos_datalist,
+  initparsopt = c(2, 2.7, 20, 0.009, 1.01),
+  idparsopt = 1:5,
+  parsfix = 1,
+  idparsfix = 6,
+  ddmodel = 11,
+  optimmethod = "simplex",
+  CS_version = create_CS_version(model = 2,
+                                 relaxed_par = "cladogenesis"))
 
-relaxed_clado_sd_01 <- DAISIE_ML_CS(datalist = Galapagos_datalist,
-                                    initparsopt = c(2, 2.7, 20, 0.009, 1.01),
-                                    idparsopt = 1:5,
-                                    parsfix = NULL,
-                                    idparsfix = NULL,
-                                    ddmodel = 11,
-                                    CS_version = -2.01)
 
-relaxed_clado_sd_001 <- DAISIE_ML_CS(datalist = Galapagos_datalist,
-                                     initparsopt = c(2, 2.7, 20, 0.009, 1.01),
-                                     idparsopt = 1:5,
-                                     parsfix = NULL,
-                                     idparsfix = NULL,
-                                     ddmodel = 11,
-                                     CS_version = -2.001)
+rr_clado_sd_001 <- DAISIE_ML_CS(
+  datalist = Galapagos_datalist,
+  initparsopt = c(2, 2.7, 20, 0.009, 1.01),
+  idparsopt = 1:5,
+  parsfix = 0.01,
+  idparsfix = 6,
+  ddmodel = 11,
+  optimmethod = "simplex",
+  CS_version = create_CS_version(model = 2,
+                                 relaxed_par = "cladogenesis"))
 
-relaxed_clado_sd_0001 <- DAISIE_ML_CS(datalist = Galapagos_datalist,
-                                      initparsopt = c(2, 2.7, 20, 0.009, 1.01),
-                                      idparsopt = 1:5,
-                                      parsfix = NULL,
-                                      idparsfix = NULL,
-                                      ddmodel = 11,
-                                      CS_version = -2.0001)
-
-relaxed_clado_sd_00001 <- DAISIE_ML_CS(datalist = Galapagos_datalist,
-                                       initparsopt = c(2, 2.7, 20, 0.009, 1.01),
-                                       idparsopt = 1:5,
-                                       parsfix = NULL,
-                                       idparsfix = NULL,
-                                       ddmodel = 11,
-                                       CS_version = -2.00001)
+rr_clado_sd_00001 <- DAISIE_ML_CS(
+  datalist = Galapagos_datalist,
+  initparsopt = c(2, 2.7, 20, 0.009, 1.01),
+  idparsopt = 1:5,
+  parsfix = 0.0001,
+  idparsfix = 6,
+  ddmodel = 11,
+  optimmethod = "simplex",
+  CS_version = create_CS_version(model = 2,
+                                 relaxed_par = "cladogenesis"))
