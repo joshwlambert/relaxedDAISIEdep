cabf55c0ddf1de8cd124d4f3c3e1a651d718e860
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-26 13:17:04 +0200

    edited run_rr_approach_cr.sh

--- bash/run_rr_approach_cr.sh
+++ bash/run_rr_approach_cr.sh
@@ -4,8 +4,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=rr_approx_cr
-#SBATCH --output=rr_approx_cr.out
+#SBATCH --output=logs/rr_approx_cr.log
 #SBATCH --mem=5GB
 
+mkdir -p results
+mkdir -p logs
+
 module load R
-Rscript run_rr_approx_cr.R
+Rscript run_rr_approach_cr.R
