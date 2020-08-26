7b4d16329da81943bd9fa9abe79de220df4a8fa6
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-26 13:20:01 +0200

    added run_Aldabra_Group_models.sh bash script

---
+++ bash/run_Aldabra_Group_models.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=71:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Aldabra_Group_models
+#SBATCH --output=logs/Aldabra_Group_models.log
+#SBATCH --mem=5GB
+
+mkdir -p results
+mkdir -p logs
+
+module load R
+Rscript run_rr_approach_cr.R
