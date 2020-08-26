2b1f16dd04742716998f6861188fad32d215dd15
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-26 21:08:33 +0200

    added run_Palau_models bash script

---
+++ bash/run_Palau_models.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Palau_models
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Palau_models.log
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Palau_models.R
