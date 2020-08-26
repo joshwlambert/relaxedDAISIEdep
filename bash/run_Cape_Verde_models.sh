dbe523b3743bcd9244db45315e7fa536c52a672d
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-26 21:05:26 +0200

    added run_Cape_Verde_models bash script

---
+++ bash/run_Cape_Verde_models.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Cape_Verde_models
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Cape_Verde_models.log
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Cape_Verde_models.R
