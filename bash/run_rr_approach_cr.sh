2a57afc89049c8bc2e9872b5ce68f776383082e2
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-26 11:12:07 +0200

    edited and renamed run_rr_approach_cr.sh bash script

--- bash/run_rr_approach_cr.sh
+++ bash/run_rr_approach_cr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --time=71:00:00 --partition=gelifes
+#SBATCH --time=71:00:00
+#SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=rr_approx_cr
 #SBATCH --output=rr_approx_cr.out
 #SBATCH --mem=5GB
 
-ml R/3.6.1-foss-2018a
+module load R
 Rscript run_rr_approx_cr.R
