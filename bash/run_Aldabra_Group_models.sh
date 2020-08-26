ca4490854585e6cce241fd9ce1e0f7ee000efd48
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-26 14:14:08 +0200

    removed mkdir functions from run_Aldabra_Group_models bash script

--- bash/run_Aldabra_Group_models.sh
+++ bash/run_Aldabra_Group_models.sh
@@ -4,11 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=Aldabra_Group_models
-#SBATCH --output=logs/Aldabra_Group_models.log
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group_models.log
 #SBATCH --mem=5GB
-
-mkdir -p results
-mkdir -p logs
 
 module load R
 Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models.R
