ecb6c23e8d5b356ea63b7db3483f5dbead5842e5
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-27 11:06:50 +0200

    added relaxed-rate models and saving results to Azores_models script

--- scripts/Azores_models.R
+++ scripts/Azores_models.R
@@ -53,9 +53,11 @@ for (i in seq_along(init_param_set)) {
 #Extract global optima
 loglik <- sapply(Azores_cr_di, '[[', 6)
 max_loglik <- which(max(loglik) == loglik)
+Azores_cr_di_max_loglik <- Azores_cr_di[[max_loglik]]
 
 #Calculate BIC
-BIC <- 4 * (log(1000) + log(2 * pi)) - 2 * Azores_cr_di[[max_loglik]]$loglik
+Azores_cr_di_BIC <-
+  4 * (log(1000) + log(2 * pi)) - 2 * Azores_cr_di[[max_loglik]]$loglik
 
 ### Constant rate diversity-dependent model ###
 
@@ -98,9 +100,11 @@ for (i in seq_along(init_param_set)) {
 #Extract global optima
 loglik <- sapply(Azores_cr_dd, '[[', 6)
 max_loglik <- which(max(loglik) == loglik)
+Azores_cr_dd_max_loglik <- Azores_cr_dd[[max_loglik]]
 
 #Calculate BIC
-BIC <- 5 * (log(1000) + log(2 * pi)) - 2 * Azores_cr_dd[[max_loglik]]$loglik
+Azores_cr_dd_BIC <-
+  5 * (log(1000) + log(2 * pi)) - 2 * Azores_cr_dd[[max_loglik]]$loglik
 
 ### Constant rate diversity-independent no cladogenesis model ###
 
@@ -139,9 +143,12 @@ for (i in seq_along(init_param_set)) {
 #Extract global optima
 loglik <- sapply(Azores_cr_di_no_lambda_c, '[[', 6)
 max_loglik <- which(max(loglik) == loglik)
+Azores_cr_di_no_lambda_c_max_loglik <- Azores_cr_di_no_lambda_c[[max_loglik]]
 
 #Calculate BIC
-BIC <- 3 * (log(1000) + log(2 * pi)) - 2 * Azores_cr_di_no_lambda_c[[max_loglik]]$loglik
+Azores_cr_di_no_lambda_c_BIC <-
+  3 * (log(1000) + log(2 * pi)) -
+  2 * Azores_cr_di_no_lambda_c[[max_loglik]]$loglik
 
 ### Constant rate diversity-dependent no cladogenesis model ###
 
@@ -182,6 +189,287 @@ for (i in seq_along(init_param_set)) {
 #Extract global optima
 loglik <- sapply(Azores_cr_dd_no_lambda_c, '[[', 6)
 max_loglik <- which(max(loglik) == loglik)
+Azores_cr_dd_no_lambda_c_max_loglik <- Azores_cr_dd_no_lambda_c[[max_loglik]]
 
 #Calculate BIC
-BIC <- 4 * (log(1000) + log(2 * pi)) - 2 * Azores_cr_dd_no_lambda_c[[max_loglik]]$loglik
+Azores_cr_dd_no_lambda_c_BIC <-
+  4 * (log(1000) + log(2 * pi)) -
+  2 * Azores_cr_dd_no_lambda_c[[max_loglik]]$loglik
+
+### Relaxed rate extinction diversity-independent no cladogenesis model ###
+
+Azores_rr_mu_di_no_lambda_c_init <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_mu_di_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    1),
+    idparsopt = c(2, 4, 5, 6),
+    parsfix = c(0, Inf),
+    idparsfix = c(1, 3),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "extinction"))
+}
+
+Azores_rr_mu_di_no_lambda_c <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_mu_di_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(Azores_rr_mu_di_no_lambda_c_init[[i]]$mu,
+                    Azores_rr_mu_di_no_lambda_c_init[[i]]$gamma,
+                    Azores_rr_mu_di_no_lambda_c_init[[i]]$lambda_a,
+                    Azores_rr_mu_di_no_lambda_c_init[[i]]$sd),
+    idparsopt = c(2, 4, 5, 6),
+    parsfix = c(0, Inf),
+    idparsfix = c(1, 3),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "extinction"))
+}
+
+#Extract global optima
+loglik <- sapply(Azores_rr_mu_di_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Azores_rr_mu_di_no_lambda_c_max_loglik <-
+  Azores_rr_mu_di_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Azores_rr_mu_di_no_lambda_c_BIC <-
+  4 * (log(1000) + log(2 * pi)) -
+  2 * Azores_rr_mu_di_no_lambda_c[[max_loglik]]$loglik
+
+### Relaxed rate extinction diversity-dependent no cladogenesis model ###
+
+Azores_rr_mu_dd_no_lambda_c_init <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_mu_dd_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    1),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "extinction"))
+}
+
+Azores_rr_mu_dd_no_lambda_c <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_mu_dd_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(Azores_rr_mu_dd_no_lambda_c_init[[i]]$mu,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$K,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$gamma,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$lambda_a,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$sd),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "extinction"))
+}
+
+#Extract global optima
+loglik <- sapply(Azores_rr_mu_dd_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Azores_rr_mu_dd_no_lambda_c_max_loglik <-
+  Azores_rr_mu_dd_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Azores_rr_mu_dd_no_lambda_c_BIC <-
+  5 * (log(1000) + log(2 * pi)) -
+  2 * Azores_rr_mu_dd_no_lambda_c[[max_loglik]]$loglik
+
+### Relaxed rate carrying capacity no cladogenesis model ###
+
+Azores_rr_k_no_lambda_c_init <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_k_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    1),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "carrying_capacity"))
+}
+
+Azores_rr_k_no_lambda_c <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_k_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(Azores_rr_mu_dd_no_lambda_c_init[[i]]$mu,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$K,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$gamma,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$lambda_a,
+                    Azores_rr_mu_dd_no_lambda_c_init[[i]]$sd),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "carrying_capacity"))
+}
+
+#Extract global optima
+loglik <- sapply(Azores_rr_k_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Azores_rr_k_no_lambda_c_max_loglik <-
+  Azores_rr_k_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Azores_rr_k_no_lambda_c_BIC <-
+  5 * (log(1000) + log(2 * pi)) -
+  2 * Azores_rr_k_no_lambda_c[[max_loglik]]$loglik
+
+### Relaxed rate anagenesis diversity-independent no cladogenesis model ###
+
+Azores_rr_lambda_a_di_no_lambda_c_init <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_lambda_a_di_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    1),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "anagenesis"))
+}
+
+Azores_rr_lambda_a_di_no_lambda_c <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_lambda_a_di_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(Azores_rr_lambda_a_di_no_lambda_c_init[[i]]$mu,
+                    Azores_rr_lambda_a_di_no_lambda_c_init[[i]]$K,
+                    Azores_rr_lambda_a_di_no_lambda_c_init[[i]]$gamma,
+                    Azores_rr_lambda_a_di_no_lambda_c_init[[i]]$lambda_a,
+                    Azores_rr_lambda_a_di_no_lambda_c_init[[i]]$sd),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "anagenesis"))
+}
+
+#Extract global optima
+loglik <- sapply(Azores_rr_lambda_a_di_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Azores_rr_lambda_a_di_no_lambda_c_max_loglik <-
+  Azores_rr_lambda_a_di_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Azores_rr_lambda_a_di_no_lambda_c_BIC <-
+  5 * (log(1000) + log(2 * pi)) -
+  2 * Azores_rr_lambda_a_di_no_lambda_c[[max_loglik]]$loglik
+
+### Relaxed rate anagenesis diversity-dependent no cladogenesis model ###
+
+Azores_rr_lambda_a_dd_no_lambda_c_init <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_lambda_a_dd_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    1),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "anagenesis"))
+}
+
+Azores_rr_lambda_a_dd_no_lambda_c <- list()
+
+for (i in seq_along(init_param_set)) {
+  Azores_rr_lambda_a_dd_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Azores,
+    initparsopt = c(Azores_rr_lambda_a_dd_no_lambda_c_init[[i]]$mu,
+                    Azores_rr_lambda_a_dd_no_lambda_c_init[[i]]$K,
+                    Azores_rr_lambda_a_dd_no_lambda_c_init[[i]]$gamma,
+                    Azores_rr_lambda_a_dd_no_lambda_c_init[[i]]$lambda_a,
+                    Azores_rr_lambda_a_dd_no_lambda_c_init[[i]]$sd),
+    idparsopt = c(2, 3, 4, 5, 6),
+    parsfix = 0,
+    idparsfix = 1,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "anagenesis"))
+}
+
+#Extract global optima
+loglik <- sapply(Azores_rr_lambda_a_dd_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Azores_rr_lambda_a_dd_no_lambda_c_max_loglik <-
+  Azores_rr_lambda_a_dd_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Azores_rr_lambda_a_dd_no_lambda_c_BIC <-
+  5 * (log(1000) + log(2 * pi)) -
+  2 * Azores_rr_lambda_a_dd_no_lambda_c[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Azores.RData")
+Azores_results <- list(
+  Azores_cr_di_max_loglik = Azores_cr_di_max_loglik,
+  Azores_cr_di_BIC = Azores_cr_di_BIC,
+  Azores_cr_dd_max_loglik = Azores_cr_dd_max_loglik,
+  Azores_cr_dd_BIC = Azores_cr_dd_BIC,
+  Azores_cr_di_no_lambda_c_max_loglik = Azores_cr_di_no_lambda_c_max_loglik,
+  Azores_cr_di_BIC = Azores_cr_di_BIC,
+  Azores_cr_dd_no_lambda_c_max_loglik = Azores_cr_dd_no_lambda_c_max_loglik,
+  Azores_cr_dd_no_lambda_c_BIC = Azores_cr_dd_no_lambda_c_BIC,
+  Azores_rr_mu_di_no_lambda_c_max_loglik = Azores_rr_mu_di_no_lambda_c_max_loglik,
+  Azores_rr_mu_di_no_lambda_c_BIC = Azores_rr_mu_di_no_lambda_c_BIC,
+  Azores_rr_mu_dd_no_lambda_c_max_loglik = Azores_rr_mu_dd_no_lambda_c_max_loglik,
+  Azores_rr_mu_dd_no_lambda_c_BIC = Azores_rr_mu_dd_no_lambda_c_BIC,
+  Azores_rr_k_no_lambda_c_max_loglik = Azores_rr_k_no_lambda_c_max_loglik,
+  Azores_rr_k_no_lambda_c_BIC = Azores_rr_k_no_lambda_c_BIC,
+  Azores_rr_lambda_a_di_no_lambda_c_max_loglik = Azores_rr_lambda_a_di_no_lambda_c_max_loglik,
+  Azores_rr_lambda_a_di_no_lambda_c_BIC = Azores_rr_lambda_a_di_no_lambda_c_BIC,
+  Azores_rr_lambda_a_dd_no_lambda_c_max_loglik = Azores_rr_lambda_a_dd_no_lambda_c_max_loglik,
+  Azores_rr_lambda_a_dd_no_lambda_c_BIC = Azores_rr_lambda_a_dd_no_lambda_c_BIC)
+
+save(Azores_results,
+     file = results_file_path)
