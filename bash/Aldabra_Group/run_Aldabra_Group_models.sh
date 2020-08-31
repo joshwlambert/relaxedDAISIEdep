2ae86b19be7c3737bed8acfb2d153393b55a2c72
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-31 23:02:28 +0200

    added job array to run_Aldabra_Group_models bash script

--- bash/Aldabra_Group/run_Aldabra_Group_models.sh
+++ bash/Aldabra_Group/run_Aldabra_Group_models.sh
@@ -3,9 +3,11 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Aldabra_Group_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group_models.log
+#SBATCH --job-name=Aldabra_Group
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group%a.log
+#SBATCH --array=0-4
 #SBATCH --mem=5GB
 
 module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models.R
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c rr_lambda_a_dd_no_lambda_c)
+Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models/Aldabra_Group_${models[SLURM_ARRAY_TASK_ID]}.R
