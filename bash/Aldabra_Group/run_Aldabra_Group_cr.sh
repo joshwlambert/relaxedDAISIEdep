eb93cc3fc8cbd49956749729cb4651fb98ec9978
joshwlambert <j.w.l.lambert@rug.nl>
2020-08-31 22:16:25 +0200

    edited Aldabra_Group_cr job array test script

--- bash/Aldabra_Group/run_Aldabra_Group_cr.sh
+++ bash/Aldabra_Group/run_Aldabra_Group_cr.sh
@@ -5,10 +5,12 @@
 #SBATCH --ntasks=1
 #SBATCH --job-name=Aldabra_Group_cr
 #SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group_cr.log
-#SBATCH --array=(cr_di, cr_dd, cr_di_no_lambda_c, cr_dd_no_lambda_c)
+#SBATCH --array=1-4
 #SBATCH --mem=5GB
 
 module load R
-#for i in "${array[@]}"; do echo "$i"; done
-echo Aldabra_Group_${SLURM_ARRAY_TASK_ID}
-Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models/Aldabra_Group_${SLURM_ARRAY_TASK_ID}.R
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c rr_lambda_a_dd_no_lambda_c)
+for index in "${!models[@]}";
+do
+Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models/Aldabra_Group_${models["$index"]}.R
+done
