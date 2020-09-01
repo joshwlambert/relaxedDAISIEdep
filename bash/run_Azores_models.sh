b76bd3ae3b67829ea177dc984cd996bfab8ccda6
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-01 16:51:31 +0200

    added job array to run_Azores_models bash script

--- bash/run_Azores_models.sh
+++ bash/run_Azores_models.sh
@@ -3,9 +3,13 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Azores_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Azores_models.log
+#SBATCH --job-name=Azores
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Azores%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
+mkdir -p results
+mkdir -p results/Azores
 module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Azores_models.R
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c rr_lambda_a_dd_no_lambda_c)
+Rscript /home/p287218/relaxedDAISIE/scripts/Azores_models/Azores_Group_${models[SLURM_ARRAY_TASK_ID]}.R
