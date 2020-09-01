7dd80c557f502e863aee145853034ff8f739f8d8
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-01 17:37:50 +0200

    finished Canaries_rr_mu_di script

---
+++ scripts/Canaries_models/Canaries_rr_mu_di.R
@@ -0,0 +1,70 @@
+# Canaries relaxed rate extinction diversity-independent model
+
+library(DAISIE)
+
+data(Canaries)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Canaries_rr_mu_di_init <- list()
+Canaries_rr_mu_di <- list()
+
+for (i in seq_along(init_param_set)) {
+  Canaries_rr_mu_di_init[[i]] <- DAISIE_ML_CS(
+    datalist = Canaries,
+    initparsopt = c(init_param_set[[i]][1],
+                    init_param_set[[i]][2],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    init_param_set[[i]][6]),
+    idparsopt = c(1, 2, 4, 5, 6),
+    parsfix = Inf,
+    idparsfix = 3,
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "extinction"))
+
+  Canaries_rr_mu_di[[i]] <- DAISIE_ML_CS(
+    datalist = Canaries,
+    initparsopt = c(Canaries_rr_mu_di_init[[i]]$lambda_c,
+                    Canaries_rr_mu_di_init[[i]]$mu,
+                    Canaries_rr_mu_di_init[[i]]$gamma,
+                    Canaries_rr_mu_di_init[[i]]$lambda_a,
+                    Canaries_rr_mu_di_init[[i]]$sd),
+    idparsopt = c(1, 2, 4, 5, 6),
+    parsfix = Inf,
+    idparsfix = 3,
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "extinction"))
+}
+
+#Extract global optima
+loglik <- sapply(Canaries_rr_mu_di, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Canaries_rr_mu_di_max_loglik <-
+  Canaries_rr_mu_di[[max_loglik]]
+
+#Calculate BIC
+Canaries_rr_mu_di_BIC <-
+  4 * (log(1000) + log(2 * pi)) - 2 * Canaries_rr_mu_di[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Canaries",
+                               "Canaries_rr_mu_di.RData")
+Canaries_rr_mu_di_results <- list(
+  Canaries_rr_mu_di_max_loglik = Canaries_rr_mu_di_max_loglik,
+  Canaries_rr_mu_di_BIC = Canaries_rr_mu_di_BIC)
+
+save(Canaries_rr_mu_di_results,
+     file = results_file_path)
