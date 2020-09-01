b49b8f77110baeb2e02d89cc4833a73932cf5e70
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-01 11:27:47 +0200

    added mkdir functions to run_Aldabra_Group_models bash script to make results and island results folder

--- bash/Aldabra_Group/run_Aldabra_Group_models.sh
+++ bash/Aldabra_Group/run_Aldabra_Group_models.sh
@@ -8,6 +8,8 @@
 #SBATCH --array=0-4
 #SBATCH --mem=5GB
 
+mkdir -p results
+mkdir -p results/Aldabra_Group
 module load R
 models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c rr_lambda_a_dd_no_lambda_c)
 Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models/Aldabra_Group_${models[SLURM_ARRAY_TASK_ID]}.R
