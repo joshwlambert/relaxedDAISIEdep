cd40a63d7f54e26221a032b006bca3d8f4eee140
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-03 13:58:13 +0200

    added test_gamma scripts

---
+++ scripts/test_rr_gamma/test_gamma_1.R
@@ -0,0 +1,52 @@
+# Testing whether relaxed gamma can be estimated
+
+library(DAISIE)
+
+clado <- 1.0
+ext <- 1.0
+carr_cap <- c(10, Inf)
+immig <- c(0.01, 0.05)
+ana <- 1.0
+sd <- c(0.01, 0.05)
+
+param_space <- expand.grid(clado = clado,
+                           ext = ext,
+                           carr_cap = carr_cap,
+                           immig = immig,
+                           ana = ana,
+                           sd = sd)
+
+#Simulate with relaxed-rate models
+
+set.seed(1)
+
+rr_gamma_sim_data <- DAISIE_sim_relaxed_rate(
+    time = 5,
+    M = 1000,
+    pars = as.numeric(param_space[1, ]),
+    replicates = 2,
+    relaxed_par = "immigration")
+
+rr_gamma_ml <- list()
+for (i in seq_alongrr_gamma_sim_data) {
+  rr_gamma_ml[[i]] <- DAISIE_ML_CS(
+    datalist = rr_gamma_sim_data[[i]],
+    initparsopt = as.numeric(param_space[1, ]),
+    idparsopt = 1:6,
+    idparsfix = NULL,
+    parsfix = NULL,
+    optimmethod = "simplex",
+    CS_version = create_CS_version(
+      model = 2,
+      pick_parameter = "carrying_capacity"))
+}
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "test_gamma",
+                               "test_gamma_1.RData")
+test_gamma_1 <- list(
+  rr_gamma_sim_data = rr_gamma_sim_data,
+  rr_gamma_ml = rr_gamma_ml)
+
+save(test_gamma_1,
+     file = results_file_path)
