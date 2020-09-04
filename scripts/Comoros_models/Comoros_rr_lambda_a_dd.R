8335a1e2543e16924894b60e53f7556b5c79df07
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-04 20:34:00 +0200

    finished Comoros_rr_lambda_a_dd script

---
+++ scripts/Comoros_models/Comoros_rr_lambda_a_dd.R
@@ -0,0 +1,73 @@
+# Comoros relaxed rate anagenesis diversity-dependent model
+
+library(DAISIE)
+
+data(Comoros)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Comoros_rr_lambda_a_dd_init <- list()
+Comoros_rr_lambda_a_dd <- list()
+
+for (i in seq_along(init_param_set)) {
+  Comoros_rr_lambda_a_dd_init[[i]] <- DAISIE_ML_CS(
+    datalist = Comoros,
+    initparsopt = c(init_param_set[[i]][1],
+                    init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    init_param_set[[i]][6]),
+    idparsopt = c(1, 2, 3, 4, 5, 6),
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "anagenesis"))
+
+  Comoros_rr_lambda_a_dd[[i]] <- DAISIE_ML_CS(
+    datalist = Comoros,
+    initparsopt = c(Comoros_rr_lambda_a_dd_init[[i]]$lambda_c,
+                    Comoros_rr_lambda_a_dd_init[[i]]$mu,
+                    Comoros_rr_lambda_a_dd_init[[i]]$K,
+                    Comoros_rr_lambda_a_dd_init[[i]]$gamma,
+                    Comoros_rr_lambda_a_dd_init[[i]]$lambda_a,
+                    Comoros_rr_lambda_a_dd_init[[i]]$sd),
+    idparsopt = c(1, 2, 3, 4, 5, 6),
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "anagenesis"))
+}
+
+#Extract global optima
+loglik <- sapply(Comoros_rr_lambda_a_dd, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Comoros_rr_lambda_a_dd_max_loglik <-
+  Comoros_rr_lambda_a_dd[[max_loglik]]
+
+#Calculate BIC
+Comoros_rr_lambda_a_dd_BIC <-
+  5 * (log(1000) + log(2 * pi)) -
+  2 * Comoros_rr_lambda_a_dd[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Comoros",
+                               "Comoros_rr_lambda_a_dd.RData")
+Comoros_rr_lambda_a_dd_results <- list(
+  Comoros_rr_lambda_a_dd_max_loglik = Comoros_rr_lambda_a_dd_max_loglik,
+  Comoros_rr_lambda_a_dd_BIC = Comoros_rr_lambda_a_dd_BIC)
+
+save(Comoros_rr_lambda_a_dd_results,
+     file = results_file_path)
