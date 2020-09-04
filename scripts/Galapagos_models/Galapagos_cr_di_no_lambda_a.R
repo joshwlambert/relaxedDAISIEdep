0535afde3f94bfa322a923b7cf715c18ff7ec9c6
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-04 20:58:06 +0200

    finished Galapagos_cr_di_no_lambda_a script

---
+++ scripts/Galapagos_models/Galapagos_cr_di_no_lambda_a.R
@@ -0,0 +1,65 @@
+# Galapagos constant rate diversity-independent no anagenesis model
+
+library(DAISIE)
+
+data(Galapagos)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Galapagos_cr_di_no_lambda_a_init <- list()
+Galapagos_cr_di_no_lambda_a <- list()
+
+for (i in seq_along(init_param_set)) {
+  Galapagos_cr_di_no_lambda_a_init[[i]] <- DAISIE_ML_CS(
+    datalist = Galapagos,
+    initparsopt = c(init_param_set[[i]][1],
+                    init_param_set[[i]][2],
+                    init_param_set[[i]][4]),
+    idparsopt = c(1, 2, 4),
+    parsfix = c(Inf, 0),
+    idparsfix = c(3, 5),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = 1)
+
+  Galapagos_cr_di_no_lambda_a[[i]] <- DAISIE_ML_CS(
+    datalist = Galapagos,
+    initparsopt = c(Galapagos_cr_di_no_lambda_a_init[[i]]$lambda_c,
+                    Galapagos_cr_di_no_lambda_a_init[[i]]$mu,
+                    Galapagos_cr_di_no_lambda_a_init[[i]]$gamma),
+    idparsopt = c(1, 2, 4),
+    parsfix = c(Inf, 0),
+    idparsfix = c(3, 5),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = 1)
+}
+
+#Extract global optima
+loglik <- sapply(Galapagos_cr_di_no_lambda_a, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Galapagos_cr_di_no_lambda_a_max_loglik <-
+  Galapagos_cr_di_no_lambda_a[[max_loglik]]
+
+#Calculate BIC
+Galapagos_cr_di_no_lambda_a_BIC <-
+  3 * (log(1000) + log(2 * pi)) -
+  2 * Galapagos_cr_di_no_lambda_a[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Galapagos",
+                               "Galapagos_cr_di_no_lambda_a.RData")
+Galapagos_cr_di_no_lambda_a_results <- list(
+  Galapagos_cr_di_no_lambda_a_max_loglik = Galapagos_cr_di_no_lambda_a_max_loglik,
+  Galapagos_cr_di_no_lambda_a_BIC = Galapagos_cr_di_no_lambda_a_BIC)
+
+save(Galapagos_cr_di_no_lambda_a_results,
+     file = results_file_path)
