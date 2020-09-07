10b9f45f2782f66bc34f85500fc7f74c5de8d5bc
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-07 13:40:31 +0200

    finished Hawaii_rr_k script

---
+++ scripts/Hawaii_models/Hawaii_rr_k.R
@@ -0,0 +1,72 @@
+# Hawaii relaxed rate carrying capacity model
+
+library(DAISIE)
+
+data(Hawaii)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Hawaii_rr_k_init <- list()
+Hawaii_rr_k <- list()
+
+for (i in seq_along(init_param_set)) {
+  Hawaii_rr_k_init[[i]] <- DAISIE_ML_CS(
+    datalist = Hawaii,
+    initparsopt = c(init_param_set[[i]][1],
+                    init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5],
+                    init_param_set[[i]][6]),
+    idparsopt = c(1, 2, 3, 4, 5, 6),
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "carrying_capacity"))
+
+  Hawaii_rr_k[[i]] <- DAISIE_ML_CS(
+    datalist = Hawaii,
+    initparsopt = c(Hawaii_rr_k_init[[i]]$lambda_c,
+                    Hawaii_rr_k_init[[i]]$mu,
+                    Hawaii_rr_k_init[[i]]$K,
+                    Hawaii_rr_k_init[[i]]$gamma,
+                    Hawaii_rr_k_init[[i]]$lambda_a,
+                    Hawaii_rr_k_init[[i]]$sd),
+    idparsopt = c(1, 2, 3, 4, 5, 6),
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = DAISIE::create_CS_version(model = 2,
+                                           relaxed_par = "carrying_capacity"))
+}
+
+#Extract global optima
+loglik <- sapply(Hawaii_rr_k, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Hawaii_rr_k_max_loglik <-
+  Hawaii_rr_k[[max_loglik]]
+
+#Calculate BIC
+Hawaii_rr_k_BIC <-
+  5 * (log(1000) + log(2 * pi)) - 2 * Hawaii_rr_k[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Hawaii",
+                               "Hawaii_rr_k.RData")
+Hawaii_rr_k_results <- list(
+  Hawaii_rr_k_max_loglik = Hawaii_rr_k_max_loglik,
+  Hawaii_rr_k_BIC = Hawaii_rr_k_BIC)
+
+save(Hawaii_rr_k_results,
+     file = results_file_path)
