c9e0f1004dafd885c279504b9f82cdc887c6b36e
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-07 12:20:55 +0200

    finished Guam_Marianas_cr_dd script

---
+++ scripts/Guam_Marianas_models/Guam_Marianas_cr_dd.R
@@ -0,0 +1,67 @@
+# Guam Marianas constant rate diversity-dependent model
+
+library(DAISIE)
+
+data(Guam_Marianas)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Guam_Marianas_cr_dd_init <- list()
+Guam_Marianas_cr_dd <- list()
+
+for (i in seq_along(init_param_set)) {
+  Guam_Marianas_cr_dd_init[[i]] <- DAISIE_ML_CS(
+    datalist = Guam_Marianas,
+    initparsopt = c(init_param_set[[i]][1],
+                    init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5]),
+    idparsopt = 1:5,
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = 1)
+
+  Guam_Marianas_cr_dd[[i]] <- DAISIE_ML_CS(
+    datalist = Guam_Marianas,
+    initparsopt = c(Guam_Marianas_cr_dd_init[[i]]$lambda_c,
+                    Guam_Marianas_cr_dd_init[[i]]$mu,
+                    Guam_Marianas_cr_dd_init[[i]]$K,
+                    Guam_Marianas_cr_dd_init[[i]]$gamma,
+                    Guam_Marianas_cr_dd_init[[i]]$lambda_a),
+    idparsopt = 1:5,
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = 1)
+}
+
+#Extract global optima
+loglik <- sapply(Guam_Marianas_cr_dd, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Guam_Marianas_cr_dd_max_loglik <- Guam_Marianas_cr_dd[[max_loglik]]
+
+#Calculate BIC
+Guam_Marianas_cr_dd_BIC <-
+  5 * (log(1000) + log(2 * pi)) - 2 * Guam_Marianas_cr_dd[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Guam_Marianas",
+                               "Guam_Marianas_cr_dd.RData")
+Guam_Marianas_cr_dd_results <- list(
+  Guam_Marianas_cr_dd_max_loglik = Guam_Marianas_cr_dd_max_loglik,
+  Guam_Marianas_cr_dd_BIC = Guam_Marianas_cr_dd_BIC)
+
+save(Guam_Marianas_cr_dd_results,
+     file = results_file_path)
