73b131589dc6f0002cc869b8cc52678c90503b84
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-07 12:25:59 +0200

    finished Guam_Marianas_cr_di_no_lambda_c script

---
+++ scripts/Guam_Marianas_models/Guam_Marianas_cr_di_no_lambda_c.R
@@ -0,0 +1,64 @@
+# Guam Marianas constant rate diversity-independent no cladogenesis model
+
+library(DAISIE)
+
+data(Guam_Marianas)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Guam_Marianas_cr_di_no_lambda_c_init <- list()
+Guam_Marianas_cr_di_no_lambda_c <- list()
+
+for (i in seq_along(init_param_set)) {
+  Guam_Marianas_cr_di_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Guam_Marianas,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5]),
+    idparsopt = c(2, 4, 5),
+    parsfix = c(0, Inf),
+    idparsfix = c(1, 3),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = 1)
+
+  Guam_Marianas_cr_di_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Guam_Marianas,
+    initparsopt = c(Guam_Marianas_cr_di_no_lambda_c_init[[i]]$mu,
+                    Guam_Marianas_cr_di_no_lambda_c_init[[i]]$gamma,
+                    Guam_Marianas_cr_di_no_lambda_c_init[[i]]$lambda_a),
+    idparsopt = c(2, 4, 5),
+    parsfix = c(0, Inf),
+    idparsfix = c(1, 3),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = 1)
+}
+
+#Extract global optima
+loglik <- sapply(Guam_Marianas_cr_di_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Guam_Marianas_cr_di_no_lambda_c_max_loglik <- Guam_Marianas_cr_di_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Guam_Marianas_cr_di_no_lambda_c_BIC <-
+  3 * (log(1000) + log(2 * pi)) -
+  2 * Guam_Marianas_cr_di_no_lambda_c[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Guam_Marianas",
+                               "Guam_Marianas_cr_di_no_lambda_c.RData")
+Guam_Marianas_cr_di_no_lambda_c_results <- list(
+  Guam_Marianas_cr_di_no_lambda_c_max_loglik = Guam_Marianas_cr_di_no_lambda_c_max_loglik,
+  Guam_Marianas_cr_di_no_lambda_c_BIC = Guam_Marianas_cr_di_no_lambda_c_BIC)
+
+save(Guam_Marianas_cr_di_no_lambda_c_results,
+     file = results_file_path)
