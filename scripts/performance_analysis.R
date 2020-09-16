36d1be861f9e2d5adfa85a34bba26ee6d8e50d33
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-16 13:58:25 +0200

    added more indexing to sim and ml in performance_analysis

--- scripts/performance_analysis.R
+++ scripts/performance_analysis.R
@@ -5,33 +5,19 @@ args <- commandArgs(TRUE)
 set.seed(as.numeric(args[1]))
 sd <- c(0.1, 1, 5)
 
+sim <- list()
+
 if (args[2] == "cr") {
-  sim <- DAISIE_sim_constant_rate(
+  sim[[1]] <- DAISIE_sim_constant_rate(
     time = 5,
     M = 1000,
     pars = c(1, 1, 30, 0.1, 1),
     replicates = 1)
 }
 
-if (args[3] == "cr") {
-  cr_ml <- DAISIE_ML_CS(
-    datalist = sim[[1]],
-    initparsopt = c(1, 1, 30, 0.01, 1),
-    idparsopt = 1:5,
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = 1)
-  results_file_path <- file.path(getwd(), "results", "performance_analysis",
-                                 "cr_ml.RData")
-  save(cr_ml,
-       file = results_file_path)
-}
-
-for (i in 1:3) {
+for (i in seq_along(sd)) {
   if (args[2] == "rr_lambda_c") {
-    sim <- DAISIE_sim_relaxed_rate(
+    sim[[i]] <- DAISIE_sim_relaxed_rate(
       time = 5,
       M = 1000,
       pars = c(1, 1, 30, 0.1, 1, sd[i]),
@@ -39,7 +25,7 @@ for (i in 1:3) {
       relaxed_par = "cladogenesis")
   }
   if (args[2] == "rr_mu") {
-    sim <- DAISIE_sim_relaxed_rate(
+    sim[[i]] <- DAISIE_sim_relaxed_rate(
       time = 5,
       M = 1000,
       pars = c(1, 1, 30, 0.1, 1, sd[i]),
@@ -47,7 +33,7 @@ for (i in 1:3) {
       relaxed_par = "extinction")
   }
   if (args[2] == "rr_k") {
-    sim <- DAISIE_sim_relaxed_rate(
+    sim[[i]] <- DAISIE_sim_relaxed_rate(
       time = 5,
       M = 1000,
       pars = c(1, 1, 30, 0.1, 1, sd[i]),
@@ -55,15 +41,37 @@ for (i in 1:3) {
       relaxed_par = "carrying_capacity")
   }
   if (args[2] == "rr_lambda_a")
-    sim <- DAISIE_sim_relaxed_rate(
+    sim[[i]] <- DAISIE_sim_relaxed_rate(
       time = 5,
       M = 1000,
       pars = c(1, 1, 30, 0.1, 1, sd[i]),
       replicates = 1,
       relaxed_par = "anagenesis")
+}
 
+rr_lambda_c_ml <- list()
+rr_mu_ml <- list()
+rr_k_ml <- list()
+rr_lambda_a_ml <- list()
+
+for (i in seq_along(sim)) {
+  if (args[3] == "cr") {
+    cr_ml <- DAISIE_ML_CS(
+      datalist = sim[[i]][[1]],
+      initparsopt = c(1, 1, 30, 0.01, 1),
+      idparsopt = 1:5,
+      parsfix = NULL,
+      idparsfix = NULL,
+      ddmodel = 11,
+      optimmethod = "simplex",
+      CS_version = 1)
+    results_file_path <- file.path(getwd(), "results", "performance_analysis",
+                                   "cr_ml.RData")
+    save(cr_ml,
+         file = results_file_path)
+  }
   if (args[3] == "rr_lambda_c") {
-    rr_lambda_c_ml <- DAISIE_ML_CS(
+    rr_lambda_c_ml[[i]] <- DAISIE_ML_CS(
       datalist = sim[[1]],
       initparsopt = c(1, 1, 30, 0.01, 1, sd[i]),
       idparsopt = 1:6,
@@ -80,7 +88,7 @@ for (i in 1:3) {
          file = results_file_path)
   }
   if (args[3] == "rr_mu") {
-    rr_mu_ml <- DAISIE_ML_CS(
+    rr_mu_ml[[i]] <- DAISIE_ML_CS(
       datalist = sim[[1]],
       initparsopt = c(1, 1, 30, 0.01, 1, sd[i]),
       idparsopt = 1:6,
@@ -96,7 +104,7 @@ for (i in 1:3) {
          file = results_file_path)
   }
   if (args[3] == "rr_k") {
-    rr_k_ml <- DAISIE_ML_CS(
+    rr_k_ml[[i]] <- DAISIE_ML_CS(
       datalist = sim[[1]],
       initparsopt = c(1, 1, 30, 0.01, 1, sd[i]),
       idparsopt = 1:6,
@@ -112,7 +120,7 @@ for (i in 1:3) {
          file = results_file_path)
   }
   if (args[3] == "rr_lambda_a") {
-    rr_lambda_a_ml <- DAISIE_ML_CS(
+    rr_lambda_a_ml[[i]] <- DAISIE_ML_CS(
       datalist = sim[[1]],
       initparsopt = c(1, 1, 30, 0.01, 1, sd[i]),
       idparsopt = 1:6,
