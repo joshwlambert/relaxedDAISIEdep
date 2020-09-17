1a84f93e5a30768976ae5d3d180e18dc560a37d8
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-17 11:27:47 +0200

    added Madeira_cr_di_no_lambda_c script

---
+++ scripts/Madeira_models/Madeira_cr_di_no_lambda_c.R
@@ -0,0 +1,65 @@
+# Madeira constant rate diversity-independent no cladogenesis model
+
+library(DAISIE)
+
+data(Madeira)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Madeira_cr_di_no_lambda_c_init <- list()
+Madeira_cr_di_no_lambda_c <- list()
+
+
+for (i in seq_along(init_param_set)) {
+  Madeira_cr_di_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
+    datalist = Madeira,
+    initparsopt = c(init_param_set[[i]][2],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5]),
+    idparsopt = c(2, 4, 5),
+    parsfix = c(0, Inf),
+    idparsfix = c(1, 3),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = 1)
+
+  Madeira_cr_di_no_lambda_c[[i]] <- DAISIE_ML_CS(
+    datalist = Madeira,
+    initparsopt = c(Madeira_cr_di_no_lambda_c_init[[i]]$mu,
+                    Madeira_cr_di_no_lambda_c_init[[i]]$gamma,
+                    Madeira_cr_di_no_lambda_c_init[[i]]$lambda_a),
+    idparsopt = c(2, 4, 5),
+    parsfix = c(0, Inf),
+    idparsfix = c(1, 3),
+    ddmodel = 0,
+    optimmethod = "simplex",
+    CS_version = 1)
+}
+
+#Extract global optima
+loglik <- sapply(Madeira_cr_di_no_lambda_c, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Madeira_cr_di_no_lambda_c_max_loglik <- Madeira_cr_di_no_lambda_c[[max_loglik]]
+
+#Calculate BIC
+Madeira_cr_di_no_lambda_c_BIC <-
+  3 * (log(1000) + log(2 * pi)) -
+  2 * Madeira_cr_di_no_lambda_c[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Madeira",
+                               "Madeira_cr_di_no_lambda_c.RData")
+Madeira_cr_di_no_lambda_c_results <- list(
+  Madeira_cr_di_no_lambda_c_max_loglik = Madeira_cr_di_no_lambda_c_max_loglik,
+  Madeira_cr_di_no_lambda_c_BIC = Madeira_cr_di_no_lambda_c_BIC)
+
+save(Madeira_cr_di_no_lambda_c_results,
+     file = results_file_path)
