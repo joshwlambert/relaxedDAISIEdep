40c8d36ea3e37301c4c33641cef616a0881e141d
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-17 12:03:48 +0200

    added Marquesas_cr_dd script

---
+++ scripts/Marquesas_models/Marquesas_cr_dd.R
@@ -0,0 +1,67 @@
+# Marquesas constant rate diversity-dependent model
+
+library(DAISIE)
+
+data(Marquesas)
+
+init_param_set <- list()
+set.seed(1)
+for (i in 1:10) {
+  init_param_set[[i]] <- runif(2, 0.1, 2)
+  init_param_set[[i]][3] <- runif(1, 30, 100)
+  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
+  init_param_set[[i]][5] <- runif(1, 0.1, 2)
+  init_param_set[[i]][6] <- runif(1, 0.1, 1)
+}
+
+Marquesas_cr_dd_init <- list()
+Marquesas_cr_dd <- list()
+
+for (i in seq_along(init_param_set)) {
+  Marquesas_cr_dd_init[[i]] <- DAISIE_ML_CS(
+    datalist = Marquesas,
+    initparsopt = c(init_param_set[[i]][1],
+                    init_param_set[[i]][2],
+                    init_param_set[[i]][3],
+                    init_param_set[[i]][4],
+                    init_param_set[[i]][5]),
+    idparsopt = 1:5,
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = 1)
+
+  Marquesas_cr_dd[[i]] <- DAISIE_ML_CS(
+    datalist = Marquesas,
+    initparsopt = c(Marquesas_cr_dd_init[[i]]$lambda_c,
+                    Marquesas_cr_dd_init[[i]]$mu,
+                    Marquesas_cr_dd_init[[i]]$K,
+                    Marquesas_cr_dd_init[[i]]$gamma,
+                    Marquesas_cr_dd_init[[i]]$lambda_a),
+    idparsopt = 1:5,
+    parsfix = NULL,
+    idparsfix = NULL,
+    ddmodel = 11,
+    optimmethod = "simplex",
+    CS_version = 1)
+}
+
+#Extract global optima
+loglik <- sapply(Marquesas_cr_dd, '[[', 6)
+max_loglik <- which(max(loglik) == loglik)
+Marquesas_cr_dd_max_loglik <- Marquesas_cr_dd[[max_loglik]]
+
+#Calculate BIC
+Marquesas_cr_dd_BIC <-
+  5 * (log(1000) + log(2 * pi)) - 2 * Marquesas_cr_dd[[max_loglik]]$loglik
+
+#Save results
+results_file_path <- file.path(getwd(), "results", "Marquesas",
+                               "Marquesas_cr_dd.RData")
+Marquesas_cr_dd_results <- list(
+  Marquesas_cr_dd_max_loglik = Marquesas_cr_dd_max_loglik,
+  Marquesas_cr_dd_BIC = Marquesas_cr_dd_BIC)
+
+save(Marquesas_cr_dd_results,
+     file = results_file_path)
