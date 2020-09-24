7802735ebc71b809fe5d4b5e1569f6a6a2ac6bd4
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-24 18:12:03 +0200

    added tests to archipelago_analysis

--- tests/testthat/test-archipelago_analysis.R
+++ tests/testthat/test-archipelago_analysis.R
@@ -1,3 +1,84 @@
-test_that("archipelago_analysis returns the correct ouput", {
-  expect_equal(2 * 2, 4)
+test_that("archipelago_analysis for ddmodel 11 returns the correct ouput", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+    output <- archipelago_analysis(data = Aldabra_Group,
+                                   archipelago = "Aldabra_Group",
+                                   idparsopt = 1:5,
+                                   parsfix = NULL,
+                                   idparsfix = NULL,
+                                   ddmodel = 11,
+                                   seed = 1,
+                                   relaxed_model = FALSE,
+                                   save_output = FALSE)
+expected_output <- list(ml = data.frame(lambda_c = 1.88986504583e-10,
+                                        mu = 0.652282694565,
+                                        K = 0.571806826695,
+                                        gamma = 0.0164538541001,
+                                        lambda_a = 1.01020027363,
+                                        loglik = -73.362384727,
+                                        df = 5,
+                                        conv = 0),
+                        BIC = 190.452931181)
+
+expect_equal(output, expected_output)
+  } else{
+    skip("Run only on TRAVIS and AppVeyor")
+  }
 })
+
+test_that("archipelago_analysis for ddmodel 0 returns the correct ouput", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+    output <- archipelago_analysis(data = Aldabra_Group,
+                                   archipelago = "Aldabra_Group",
+                                   idparsopt = c(1, 2, 4, 5),
+                                   parsfix = Inf,
+                                   idparsfix = 3,
+                                   ddmodel = 0,
+                                   seed = 1,
+                                   relaxed_model = FALSE,
+                                   save_output = FALSE)
+    expected_output <- list(ml = data.frame(lambda_c = 6.19057009562e-12,
+                                            mu = 0.621234136267,
+                                            K = Inf,
+                                            gamma = 0.0161468052122,
+                                            lambda_a = 1.00778686028,
+                                            loglik = -73.4180232599,
+                                            df = 4,
+                                            conv = 0),
+                            BIC = 181.818575901)
+
+    expect_equal(output, expected_output)
+  } else{
+    skip("Run only on TRAVIS and AppVeyor")
+  }
+})
+
+test_that("archipelago_analysis correctly errors when input is incorrect", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+    expect_error(archipelago_analysis(data = Aldabra_Group,
+                                      archipelago = "Aldabra_Group",
+                                      idparsopt = 1:5,
+                                      parsfix = NULL,
+                                      idparsfix = NULL,
+                                      ddmodel = 4,
+                                      seed = 1,
+                                      relaxed_model = FALSE,
+                                      save_output = FALSE))
+
+    expect_error(archipelago_analysis(data = Aldabra_Group,
+                                      archipelago = "Aldabra_Group",
+                                      idparsopt = 1:5,
+                                      parsfix = NULL,
+                                      idparsfix = NULL,
+                                      ddmodel = 11,
+                                      seed = "string",
+                                      relaxed_model = FALSE,
+                                      save_output = FALSE))
+
+  } else{
+    skip("Run only on TRAVIS and AppVeyor")
+  }
+})
+
+
+
+
