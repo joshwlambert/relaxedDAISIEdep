d1b7a982a4ababd4a1e06f2d052fa3c0dec6d554
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-24 14:17:45 +0200

    changed Aldabra_Group_rr_lambda_a_di_no_lambda_c script to new format

--- scripts/Aldabra_Group_models/Aldabra_Group_rr_lambda_a_di_no_lambda_c.R
+++ scripts/Aldabra_Group_models/Aldabra_Group_rr_lambda_a_di_no_lambda_c.R
@@ -1,71 +1,13 @@
-# Aldabra Group relaxed rate anagenesis diversity-independent no cladogenesis model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Aldabra_Group)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Aldabra_Group_rr_lambda_a_di_no_lambda_c_init <- list()
-Aldabra_Group_rr_lambda_a_di_no_lambda_c <- list()
-
-for (i in seq_along(init_param_set)) {
-  Aldabra_Group_rr_lambda_a_di_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
-    datalist = Aldabra_Group,
-    initparsopt = c(init_param_set[[i]][2],
-                    init_param_set[[i]][3],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5],
-                    init_param_set[[i]][6]),
-    idparsopt = c(2, 3, 4, 5, 6),
-    parsfix = 0,
-    idparsfix = 1,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "anagenesis"))
-
-  Aldabra_Group_rr_lambda_a_di_no_lambda_c[[i]] <- DAISIE_ML_CS(
-    datalist = Aldabra_Group,
-    initparsopt = c(Aldabra_Group_rr_lambda_a_di_no_lambda_c_init[[i]]$mu,
-                    Aldabra_Group_rr_lambda_a_di_no_lambda_c_init[[i]]$K,
-                    Aldabra_Group_rr_lambda_a_di_no_lambda_c_init[[i]]$gamma,
-                    Aldabra_Group_rr_lambda_a_di_no_lambda_c_init[[i]]$lambda_a,
-                    Aldabra_Group_rr_lambda_a_di_no_lambda_c_init[[i]]$sd),
-    idparsopt = c(2, 3, 4, 5, 6),
-    parsfix = 0,
-    idparsfix = 1,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "anagenesis"))
-}
-
-#Extract global optima
-loglik <- sapply(Aldabra_Group_rr_lambda_a_di_no_lambda_c, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Aldabra_Group_rr_lambda_a_di_no_lambda_c_max_loglik <-
-  Aldabra_Group_rr_lambda_a_di_no_lambda_c[[max_loglik]]
-
-#Calculate BIC
-Aldabra_Group_rr_lambda_a_di_no_lambda_c_BIC <-
-  5 * (log(1000) + log(2 * pi)) -
-  2 * Aldabra_Group_rr_lambda_a_di_no_lambda_c[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Aldabra_Group",
-                               "Aldabra_Group_rr_lambda_a_di_no_lambda_c.RData")
-Aldabra_Group_rr_lambda_a_di_no_lambda_c_results <- list(
-  Aldabra_Group_rr_lambda_a_di_no_lambda_c_max_loglik = Aldabra_Group_rr_lambda_a_di_no_lambda_c_max_loglik,
-  Aldabra_Group_rr_lambda_a_di_no_lambda_c_BIC = Aldabra_Group_rr_lambda_a_di_no_lambda_c_BIC)
-
-save(Aldabra_Group_rr_lambda_a_di_no_lambda_c_results,
-     file = results_file_path)
+archipelago_analysis(data = Aldabra_Group,
+                     archipelago = "Aldabra_Group",
+                     idparsopt = c(2, 4, 5, 6),
+                     parsfix = c(0, Inf),
+                     idparsfix = c(1, 3),
+                     ddmodel = 0,
+                     seed = args[1],
+                     relaxed_model = TRUE,
+                     relaxed_par = "anagenesis")
