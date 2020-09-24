2e1eaa68e78c4726c0fc2cc486b14e23df65bde5
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-24 17:29:17 +0200

    added run_Aldabra_Group_rr_lambda_a_di_no_lambda_c bash script

---
+++ bash/Aldabra_Group/run_Aldabra_Group_rr_lambda_a_di_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Aldabra_Group
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group_rr_lambda_a_di_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models/Aldabra_Group_rr_lambda_a_di_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
