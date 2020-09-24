e29c7c4774cee7769e77e3756d85a2f5c8cace74
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-24 12:26:43 +0200

    edited run_Aldabra_Group_models bash script to new format

--- bash/run_Aldabra_Group_models.sh
+++ bash/run_Aldabra_Group_models.sh
@@ -4,12 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=Aldabra_Group
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group%a.log
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Aldabra_Group_models%a.log
 #SBATCH --array=0-8
 #SBATCH --mem=5GB
 
 mkdir -p results
 mkdir -p results/Aldabra_Group
-module load R
 models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c rr_lambda_a_dd_no_lambda_c)
-Rscript /home/p287218/relaxedDAISIE/scripts/Aldabra_Group_models/Aldabra_Group_${models[SLURM_ARRAY_TASK_ID]}.R
+
+sbatch /home/p287218/relaxedDAISIE/bash/run_Aldabra_Group_${models[SLURM_ARRAY_TASK_ID]}_models.sh
