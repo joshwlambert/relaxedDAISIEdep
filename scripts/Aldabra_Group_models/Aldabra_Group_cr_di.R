99aa107256f8617c4e4c77987d39b83c1baf0152
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-24 14:16:27 +0200

    changed Aldabra_Group_cr_di script to new format

--- scripts/Aldabra_Group_models/Aldabra_Group_cr_di.R
+++ scripts/Aldabra_Group_models/Aldabra_Group_cr_di.R
@@ -1,65 +1,12 @@
-# Aldabra Group constant rate diversity independent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Aldabra_Group)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Aldabra_Group_cr_di_init <- list()
-Aldabra_Group_cr_di <- list()
-
-for (i in seq_along(init_param_set)) {
-  Aldabra_Group_cr_di_init[[i]] <- DAISIE_ML_CS(
-    datalist = Aldabra_Group,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5]),
-    idparsopt = c(1, 2, 4, 5),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-
-  Aldabra_Group_cr_di[[i]] <- DAISIE_ML_CS(
-    datalist = Aldabra_Group,
-    initparsopt = c(Aldabra_Group_cr_di_init[[i]]$lambda_c,
-                    Aldabra_Group_cr_di_init[[i]]$mu,
-                    Aldabra_Group_cr_di_init[[i]]$gamma,
-                    Aldabra_Group_cr_di_init[[i]]$lambda_a),
-    idparsopt = c(1, 2, 4, 5),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-}
-
-#Extract global optima
-loglik <- sapply(Aldabra_Group_cr_di, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Aldabra_Group_cr_di_max_loglik <- Aldabra_Group_cr_di[[max_loglik]]
-
-#Calculate BIC
-Aldabra_Group_cr_di_BIC <-
-  4 * (log(1000) + log(2 * pi)) - 2 * Aldabra_Group_cr_di[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Aldabra_Group",
-                               "Aldabra_Group_cr_di.RData")
-Aldabra_Group_cr_di_results <- list(
-  Aldabra_Group_cr_di_max_loglik = Aldabra_Group_cr_di_max_loglik,
-  Aldabra_Group_cr_di_BIC = Aldabra_Group_cr_di_BIC)
-
-save(Aldabra_Group_cr_di_results,
-     file = results_file_path)
+archipelago_analysis(data = Aldabra_Group,
+                     archipelago = "Aldabra_Group",
+                     idparsopt = c(1, 2, 4, 5),
+                     parsfix = Inf,
+                     idparsfix = 3,
+                     ddmodel = 0,
+                     seed = args[1],
+                     relaxed_model = FALSE)
