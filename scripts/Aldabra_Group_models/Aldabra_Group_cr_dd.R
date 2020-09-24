f52b3fb5941541c73787f6dc1291ddfd3e7a5b57
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-24 17:13:25 +0200

    explicit namespace call in Aldabra_Group_cr_dd script

--- scripts/Aldabra_Group_models/Aldabra_Group_cr_dd.R
+++ scripts/Aldabra_Group_models/Aldabra_Group_cr_dd.R
@@ -2,11 +2,11 @@ args <- commandArgs(TRUE)
 
 data(Aldabra_Group)
 
-archipelago_analysis(data = Aldabra_Group,
-                     archipelago = "Aldabra_Group",
-                     idparsopt = 1:5,
-                     parsfix = NULL,
-                     idparsfix = NULL,
-                     ddmodel = 11,
-                     seed = args[1],
-                     relaxed_model = FALSE)
+relaxedDAISIE::archipelago_analysis(data = Aldabra_Group,
+                                    archipelago = "Aldabra_Group",
+                                    idparsopt = 1:5,
+                                    parsfix = NULL,
+                                    idparsfix = NULL,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
