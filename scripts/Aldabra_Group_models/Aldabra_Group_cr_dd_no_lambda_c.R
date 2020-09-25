391077645c2f7d5d845eb3f0f512cdb81baa0a0b
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-25 10:38:12 +0200

    added model argument to Aldabra_Group_cr_dd_no_lambda_c

--- scripts/Aldabra_Group_models/Aldabra_Group_cr_dd_no_lambda_c.R
+++ scripts/Aldabra_Group_models/Aldabra_Group_cr_dd_no_lambda_c.R
@@ -4,6 +4,7 @@ data(Aldabra_Group)
 
 relaxedDAISIE::archipelago_analysis(data = Aldabra_Group,
                                     archipelago = "Aldabra_Group",
+                                    model = "cr_dd_no_lambda_c",
                                     idparsopt = 2:5,
                                     parsfix = 0,
                                     idparsfix = 1,
