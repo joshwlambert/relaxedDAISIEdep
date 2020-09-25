d7592bc85b6596839c4c8117ba901ae2708ff789
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-25 10:55:39 +0200

    updated format of Canaries_cr_di script

--- scripts/Canaries_models/Canaries_cr_di.R
+++ scripts/Canaries_models/Canaries_cr_di.R
@@ -1,66 +1,13 @@
-# Canaries constant rate diversity-independent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Canaries)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Canaries_cr_di_init <- list()
-Canaries_cr_di <- list()
-
-for (i in seq_along(init_param_set)) {
-  Canaries_cr_di_init[[i]] <- DAISIE_ML_CS(
-    datalist = Canaries,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5]),
-    idparsopt = c(1, 2, 4, 5),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-
-  Canaries_cr_di[[i]] <- DAISIE_ML_CS(
-    datalist = Canaries,
-    initparsopt = c(Canaries_cr_di_init[[i]]$lambda_c,
-                    Canaries_cr_di_init[[i]]$mu,
-                    Canaries_cr_di_init[[i]]$gamma,
-                    Canaries_cr_di_init[[i]]$lambda_a),
-    idparsopt = c(1, 2, 4, 5),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-}
-
-#Extract global optima
-loglik <- sapply(Canaries_cr_di, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Canaries_cr_di_max_loglik <- Canaries_cr_di[[max_loglik]]
-
-#Calculate BIC
-Canaries_cr_di_BIC <-
-  4 * (log(1000) + log(2 * pi)) - 2 * Canaries_cr_di[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Canaries",
-                               "Canaries_cr_di.RData")
-Canaries_cr_di_results <- list(
-  Canaries_cr_di_max_loglik = Canaries_cr_di_max_loglik,
-  Canaries_cr_di_BIC = Canaries_cr_di_BIC)
-
-save(Canaries_cr_di_results,
-     file = results_file_path)
-
+relaxedDAISIE::archipelago_analysis(data = Canaries,
+                                    archipelago = "Canaries",
+                                    model = "cr_di",
+                                    idparsopt = c(1, 2, 4, 5),
+                                    parsfix = Inf,
+                                    idparsfix = 3,
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
