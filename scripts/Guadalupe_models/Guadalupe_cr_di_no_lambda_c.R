a3604672aeebeee260524da5da7754f6810a0e1e
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-26 12:51:35 +0200

    updated format of Guadalupe_cr_di_no_lambda_c script

--- scripts/Guadalupe_models/Guadalupe_cr_di_no_lambda_c.R
+++ scripts/Guadalupe_models/Guadalupe_cr_di_no_lambda_c.R
@@ -1,64 +1,13 @@
-# Guadalupe constant rate diversity-independent no cladogenesis model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Guadalupe)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Guadalupe_cr_di_no_lambda_c_init <- list()
-Guadalupe_cr_di_no_lambda_c <- list()
-
-for (i in seq_along(init_param_set)) {
-  Guadalupe_cr_di_no_lambda_c_init[[i]] <- DAISIE_ML_CS(
-    datalist = Guadalupe,
-    initparsopt = c(init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5]),
-    idparsopt = c(2, 4, 5),
-    parsfix = c(0, Inf),
-    idparsfix = c(1, 3),
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-
-  Guadalupe_cr_di_no_lambda_c[[i]] <- DAISIE_ML_CS(
-    datalist = Guadalupe,
-    initparsopt = c(Guadalupe_cr_di_no_lambda_c_init[[i]]$mu,
-                    Guadalupe_cr_di_no_lambda_c_init[[i]]$gamma,
-                    Guadalupe_cr_di_no_lambda_c_init[[i]]$lambda_a),
-    idparsopt = c(2, 4, 5),
-    parsfix = c(0, Inf),
-    idparsfix = c(1, 3),
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-}
-
-#Extract global optima
-loglik <- sapply(Guadalupe_cr_di_no_lambda_c, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Guadalupe_cr_di_no_lambda_c_max_loglik <- Guadalupe_cr_di_no_lambda_c[[max_loglik]]
-
-#Calculate BIC
-Guadalupe_cr_di_no_lambda_c_BIC <-
-  3 * (log(1000) + log(2 * pi)) -
-  2 * Guadalupe_cr_di_no_lambda_c[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Guadaplupe",
-                               "Guadalupe_cr_di_no_lambda_c.RData")
-Guadalupe_cr_di_no_lambda_c_results <- list(
-  Guadalupe_cr_di_no_lambda_c_max_loglik = Guadalupe_cr_di_no_lambda_c_max_loglik,
-  Guadalupe_cr_di_no_lambda_c_BIC = Guadalupe_cr_di_no_lambda_c_BIC)
-
-save(Guadalupe_cr_di_no_lambda_c_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = Guadalupe,
+                                    archipelago = "Guadalupe",
+                                    model = "cr_di_no_lambda_c",
+                                    idparsopt = c(2, 4, 5),
+                                    parsfix = c(0, Inf),
+                                    idparsfix = c(1, 3),
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
