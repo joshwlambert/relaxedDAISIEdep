0a2a6d62a16a6e344ec024476da5f7b16b385ee0
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-26 15:34:59 +0200

    updated format of Madeira_cr_di script

--- scripts/Madeira_models/Madeira_cr_di.R
+++ scripts/Madeira_models/Madeira_cr_di.R
@@ -1,65 +1,13 @@
-# Madeira constant rate diversity-independent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Madeira)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Madeira_cr_di_init <- list()
-Madeira_cr_di <- list()
-
-for (i in seq_along(init_param_set)) {
-  Madeira_cr_di_init[[i]] <- DAISIE_ML_CS(
-    datalist = Madeira,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5]),
-    idparsopt = c(1, 2, 4, 5),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-
-  Madeira_cr_di[[i]] <- DAISIE_ML_CS(
-    datalist = Madeira,
-    initparsopt = c(Madeira_cr_di_init[[i]]$lambda_c,
-                    Madeira_cr_di_init[[i]]$mu,
-                    Madeira_cr_di_init[[i]]$gamma,
-                    Madeira_cr_di_init[[i]]$lambda_a),
-    idparsopt = c(1, 2, 4, 5),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = 1)
-}
-
-#Extract global optima
-loglik <- sapply(Madeira_cr_di, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Madeira_cr_di_max_loglik <- Madeira_cr_di[[max_loglik]]
-
-#Calculate BIC
-Madeira_cr_di_BIC <-
-  4 * (log(1000) + log(2 * pi)) - 2 * Madeira_cr_di[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Madeira",
-                               "Madeira_cr_di.RData")
-Madeira_cr_di_results <- list(
-  Madeira_cr_di_max_loglik = Madeira_cr_di_max_loglik,
-  Madeira_cr_di_BIC = Madeira_cr_di_BIC)
-
-save(Madeira_cr_di_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = Madeira,
+                                    archipelago = "Madeira",
+                                    model = "cr_di",
+                                    idparsopt = c(1, 2, 4, 5),
+                                    parsfix = Inf,
+                                    idparsfix = 3,
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
