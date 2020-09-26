c9086708a3271e53e2abd73d2322f458761f9544
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-26 13:46:37 +0200

    updated format of Hawaii_rr_lambda_a_di script

--- scripts/Hawaii_models/Hawaii_rr_lambda_a_di.R
+++ scripts/Hawaii_models/Hawaii_rr_lambda_a_di.R
@@ -1,71 +1,14 @@
-# Hawaii relaxed rate anagenesis diversity-independent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Hawaii)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Hawaii_rr_lambda_a_di_init <- list()
-Hawaii_rr_lambda_a_di <- list()
-
-for (i in seq_along(init_param_set)) {
-  Hawaii_rr_lambda_a_di_init[[i]] <- DAISIE_ML_CS(
-    datalist = Hawaii,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5],
-                    init_param_set[[i]][6]),
-    idparsopt = c(1, 2, 4, 5, 6),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "anagenesis"))
-
-  Hawaii_rr_lambda_a_di[[i]] <- DAISIE_ML_CS(
-    datalist = Hawaii,
-    initparsopt = c(Hawaii_rr_lambda_a_di_init[[i]]$lambda_c,
-                    Hawaii_rr_lambda_a_di_init[[i]]$mu,
-                    Hawaii_rr_lambda_a_di_init[[i]]$gamma,
-                    Hawaii_rr_lambda_a_di_init[[i]]$lambda_a,
-                    Hawaii_rr_lambda_a_di_init[[i]]$sd),
-    idparsopt = c(1, 2, 4, 5, 6),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "anagenesis"))
-}
-
-#Extract global optima
-loglik <- sapply(Hawaii_rr_lambda_a_di, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Hawaii_rr_lambda_a_di_max_loglik <-
-  Hawaii_rr_lambda_a_di[[max_loglik]]
-
-#Calculate BIC
-Hawaii_rr_lambda_a_di_BIC <-
-  5 * (log(1000) + log(2 * pi)) -
-  2 * Hawaii_rr_lambda_a_di[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Hawaii",
-                               "Hawaii_rr_lambda_a_di.RData")
-Hawaii_rr_lambda_a_di_results <- list(
-  Hawaii_rr_lambda_a_di_max_loglik = Hawaii_rr_lambda_a_di_max_loglik,
-  Hawaii_rr_lambda_a_di_BIC = Hawaii_rr_lambda_a_di_BIC)
-
-save(Hawaii_rr_lambda_a_di_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = Hawaii,
+                                    archipelago = "Hawaii",
+                                    model = "rr_lambda_a_di",
+                                    idparsopt = c(1, 2, 4, 5),
+                                    parsfix = Inf,
+                                    idparsfix = 3,
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "anagenesis")
