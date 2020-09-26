25b252fd632b8cfce411c12ceb11d9dc372785e0
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-26 13:47:01 +0200

    updated format of Hawaii_rr_lambda_c_dd script

--- scripts/Hawaii_models/Hawaii_rr_lambda_c_dd.R
+++ scripts/Hawaii_models/Hawaii_rr_lambda_c_dd.R
@@ -1,73 +1,14 @@
-# Hawaii relaxed rate cladogenesis diversity-dependent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Hawaii)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Hawaii_rr_lambda_c_dd_init <- list()
-Hawaii_rr_lambda_c_dd <- list()
-
-for (i in seq_along(init_param_set)) {
-  Hawaii_rr_lambda_c_dd_init[[i]] <- DAISIE_ML_CS(
-    datalist = Hawaii,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][3],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5],
-                    init_param_set[[i]][6]),
-    idparsopt = c(1, 2, 3, 4, 5, 6),
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "cladogenesis"))
-
-  Hawaii_rr_lambda_c_dd[[i]] <- DAISIE_ML_CS(
-    datalist = Hawaii,
-    initparsopt = c(Hawaii_rr_lambda_c_dd_init[[i]]$lambda_c,
-                    Hawaii_rr_lambda_c_dd_init[[i]]$mu,
-                    Hawaii_rr_lambda_c_dd_init[[i]]$K,
-                    Hawaii_rr_lambda_c_dd_init[[i]]$gamma,
-                    Hawaii_rr_lambda_c_dd_init[[i]]$lambda_a,
-                    Hawaii_rr_lambda_c_dd_init[[i]]$sd),
-    idparsopt = c(1, 2, 3, 4, 5, 6),
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "cladogenesis"))
-}
-
-#Extract global optima
-loglik <- sapply(Hawaii_rr_lambda_c_dd, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Hawaii_rr_lambda_c_dd <-
-  Hawaii_rr_lambda_c_dd[[max_loglik]]
-
-#Calculate BIC
-Hawaii_rr_lambda_c_dd_BIC <-
-  5 * (log(1000) + log(2 * pi)) -
-  2 * Hawaii_rr_lambda_c_dd[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Hawaii",
-                               "Hawaii_rr_lambda_c_dd.RData")
-Hawaii_rr_lambda_c_dd_results <- list(
-  Hawaii_rr_lambda_c_dd_max_loglik = Hawaii_rr_lambda_c_dd_max_loglik,
-  Hawaii_rr_lambda_c_dd_BIC = Hawaii_rr_lambda_c_dd_BIC)
-
-save(Hawaii_rr_lambda_c_dd_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = Hawaii,
+                                    archipelago = "Hawaii",
+                                    model = "rr_lambda_c_dd",
+                                    idparsopt = 1:5,
+                                    parsfix = NULL,
+                                    idparsfix = NULL,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "cladogenesis")
