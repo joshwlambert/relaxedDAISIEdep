4167448082627c4616014287f53d695596b25a52
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-26 12:42:32 +0200

    updated format of Greater_Antilles_rr_mu_di script

--- scripts/Greater_Antilles_models/Greater_Antilles_rr_mu_di.R
+++ scripts/Greater_Antilles_models/Greater_Antilles_rr_mu_di.R
@@ -1,71 +1,14 @@
-### Greater Antilles relaxed rate extinction diversity-independent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Greater_Antilles)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Greater_Antilles_rr_mu_di_init <- list()
-Greater_Antilles_rr_mu_di <- list()
-
-for (i in seq_along(init_param_set)) {
-  Greater_Antilles_rr_mu_di_init[[i]] <- DAISIE_ML_CS(
-    datalist = Greater_Antilles,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5],
-                    init_param_set[[i]][6]),
-    idparsopt = c(1, 2, 4, 5, 6),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "extinction"))
-
-  Greater_Antilles_rr_mu_di[[i]] <- DAISIE_ML_CS(
-    datalist = Greater_Antilles,
-    initparsopt = c(Greater_Antilles_rr_mu_di_init[[i]]$lambda_c,
-                    Greater_Antilles_rr_mu_di_init[[i]]$mu,
-                    Greater_Antilles_rr_mu_di_init[[i]]$gamma,
-                    Greater_Antilles_rr_mu_di_init[[i]]$lambda_a,
-                    Greater_Antilles_rr_mu_di_init[[i]]$sd),
-    idparsopt = c(1, 2, 4, 5, 6),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "extinction"))
-}
-
-#Extract global optima
-loglik <- sapply(Greater_Antilles_rr_mu_di, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Greater_Antilles_rr_mu_di_max_loglik <-
-  Greater_Antilles_rr_mu_di[[max_loglik]]
-
-#Calculate BIC
-Greater_Antilles_rr_mu_di_BIC <-
-  4 * (log(1000) + log(2 * pi)) -
-  2 * Greater_Antilles_rr_mu_di[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Greater_Antilles",
-                               "Greater_Antilles_rr_mu_di.RData")
-Greater_Antilles_rr_mu_di_results <- list(
-  Greater_Antilles_rr_mu_di_max_loglik = Greater_Antilles_rr_mu_di_max_loglik,
-  Greater_Antilles_rr_mu_di_BIC = Greater_Antilles_rr_mu_di_BIC)
-
-save(Greater_Antilles_rr_mu_di_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = Greater_Antilles,
+                                    archipelago = "Greater_Antilles",
+                                    model = "rr_mu_di",
+                                    idparsopt = c(1, 2, 4, 5, 6),
+                                    parsfix = Inf,
+                                    idparsfix = 3,
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "extinction")
