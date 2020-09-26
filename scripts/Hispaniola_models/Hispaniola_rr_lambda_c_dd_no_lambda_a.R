326b768038b177236bd064db24d036ea1a643bb3
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-26 15:27:19 +0200

    updated format of Hispaniola_rr_lambda_c_dd_no_lambda_a script

--- scripts/Hispaniola_models/Hispaniola_rr_lambda_c_dd_no_lambda_a.R
+++ scripts/Hispaniola_models/Hispaniola_rr_lambda_c_dd_no_lambda_a.R
@@ -1,71 +1,14 @@
-# Hispaniola relaxed-rate cladogenesis diversity-dependent no angenesis model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(Hispaniola)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-Hispaniola_rr_lambda_c_dd_no_lambda_a_init <- list()
-Hispaniola_rr_lambda_c_dd_no_lambda_a <- list()
-
-for (i in seq_along(init_param_set)) {
-  Hispaniola_rr_lambda_c_dd_no_lambda_a_init[[i]] <- DAISIE_ML_CS(
-    datalist = Hispaniola,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][3],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][6]),
-    idparsopt = c(1, 2, 3, 4, 6),
-    parsfix = 0,
-    idparsfix = 5,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "cladogenesis"))
-
-  Hispaniola_rr_lambda_c_dd_no_lambda_a[[i]] <- DAISIE_ML_CS(
-    datalist = Hispaniola,
-    initparsopt = c(Hispaniola_rr_lambda_c_dd_no_lambda_a_init[[i]]$lambda_c,
-                    Hispaniola_rr_lambda_c_dd_no_lambda_a_init[[i]]$mu,
-                    Hispaniola_rr_lambda_c_dd_no_lambda_a_init[[i]]$K,
-                    Hispaniola_rr_lambda_c_dd_no_lambda_a_init[[i]]$gamma,
-                    Hispaniola_rr_lambda_c_dd_no_lambda_a_init[[i]]$sd),
-    idparsopt = c(1, 2, 3, 4, 6),
-    parsfix = 0,
-    idparsfix = 5,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "cladogenesis"))
-}
-
-#Extract global optima
-loglik <- sapply(Hispaniola_rr_lambda_c_dd_no_lambda_a, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-Hispaniola_rr_lambda_c_dd_no_lambda_a_max_loglik <-
-  Hispaniola_rr_lambda_c_di_no_lambda_a[[max_loglik]]
-
-#Calculate BIC
-Hispaniola_rr_lambda_c_dd_no_lambda_a_BIC <-
-  5 * (log(1000) + log(2 * pi)) -
-  2 * Hispaniola_rr_lambda_c_dd_no_lambda_a[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "Hispaniola",
-                               "Hispaniola_rr_lambda_c_dd_no_lambda_a.RData")
-Hispaniola_rr_lambda_c_dd_no_lambda_a_results <- list(
-  Hispaniola_rr_lambda_c_dd_no_lambda_a_max_loglik = Hispaniola_rr_lambda_c_dd_no_lambda_a_max_loglik,
-  Hispaniola_rr_lambda_c_dd_no_lambda_a_BIC = Hispaniola_rr_lambda_c_dd_no_lambda_a_BIC)
-
-save(Hispaniola_rr_lambda_c_di_no_lambda_a_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = Hispaniola,
+                                    archipelago = "Hispaniola",
+                                    model = "rr_k",
+                                    idparsopt = c(1, 2, 3, 4, 6),
+                                    parsfix = 0,
+                                    idparsfix = 5,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "cladogenesis")
