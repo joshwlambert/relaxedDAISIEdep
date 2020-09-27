d61bda2a232d2cca90ea77f894f22405cf68af0f
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-27 15:48:09 +0200

    updated format of New_Caledonia_rr_lambda_c_di script

--- scripts/New_Caledonia_models/New_Caledonia_rr_lambda_c_di.R
+++ scripts/New_Caledonia_models/New_Caledonia_rr_lambda_c_di.R
@@ -1,71 +1,14 @@
-# New Caledonia relaxed rate cladogenesis diversity independent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(New_Caledonia)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-New_Caledonia_rr_lambda_c_di_init <- list()
-New_Caledonia_rr_lambda_c_di <- list()
-
-for (i in seq_along(init_param_set)) {
-  New_Caledonia_rr_lambda_c_di_init[[i]] <- DAISIE_ML_CS(
-    datalist = New_Caledonia,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5],
-                    init_param_set[[i]][6]),
-    idparsopt = c(1, 2, 4, 5, 6),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "cladogenesis"))
-
-  New_Caledonia_rr_lambda_c_di[[i]] <- DAISIE_ML_CS(
-    datalist = New_Caledonia,
-    initparsopt = c(New_Caledonia_rr_lambda_c_di_init[[i]]$lambda_c,
-                    New_Caledonia_rr_lambda_c_di_init[[i]]$mu,
-                    New_Caledonia_rr_lambda_c_di_init[[i]]$gamma,
-                    New_Caledonia_rr_lambda_c_di_init[[i]]$lambda_a,
-                    New_Caledonia_rr_lambda_c_di_init[[i]]$sd),
-    idparsopt = c(1, 2, 4, 5, 6),
-    parsfix = Inf,
-    idparsfix = 3,
-    ddmodel = 0,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "cladogenesis"))
-}
-
-#Extract global optima
-loglik <- sapply(New_Caledonia_rr_lambda_c_di, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-New_Caledonia_rr_lambda_c_di_max_loglik <-
-  New_Caledonia_rr_lambda_c_di[[max_loglik]]
-
-#Calculate BIC
-New_Caledonia_rr_lambda_c_di_BIC <-
-  4 * (log(1000) + log(2 * pi)) -
-  2 * New_Caledonia_rr_lambda_c_di[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "New_Caledonia",
-                               "New_Caledonia_rr_lambda_c_di.RData")
-New_Caledonia_rr_lambda_c_di_results <- list(
-  New_Caledonia_rr_lambda_c_di_max_loglik = New_Caledonia_rr_lambda_c_di_max_loglik,
-  New_Caledonia_rr_lambda_c_di_BIC = New_Caledonia_rr_lambda_c_di_BIC)
-
-save(New_Caledonia_rr_lambda_c_di_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = New_Caledonia,
+                                    archipelago = "New_Caledonia",
+                                    model = "rr_lambda_c_di",
+                                    idparsopt = c(1, 2, 4, 5, 6),
+                                    parsfix = Inf,
+                                    idparsfix = 3,
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "cladogenesis")
