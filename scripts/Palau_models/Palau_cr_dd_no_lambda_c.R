9ac62c51597a188ad5b57957c34bd4132f079aad
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-27 17:09:45 +0200

    added Palau_cr_dd_no_lambda_c script

---
+++ scripts/Palau_models/Palau_cr_dd_no_lambda_c.R
@@ -0,0 +1,13 @@
+args <- commandArgs(TRUE)
+
+data(Palau)
+
+relaxedDAISIE::archipelago_analysis(data = Palau,
+                                    archipelago = "Palau",
+                                    model = "cr_dd_no_lambda_c",
+                                    idparsopt = 2:5,
+                                    parsfix = 0,
+                                    idparsfix = 1,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
