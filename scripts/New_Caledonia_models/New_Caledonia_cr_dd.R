8e58703271af1a0a69d2f2358d9e4cac4587df92
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-27 15:46:21 +0200

    updated format of New_Caledonia_cr_dd script

--- scripts/New_Caledonia_models/New_Caledonia_cr_dd.R
+++ scripts/New_Caledonia_models/New_Caledonia_cr_dd.R
@@ -1,68 +1,13 @@
-# New_Caledonia constant rate diversity-dependent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(New_Caledonia)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-New_Caledonia_cr_dd_init <- list()
-New_Caledonia_cr_dd <- list()
-
-for (i in seq_along(init_param_set)) {
-  New_Caledonia_cr_dd_init[[i]] <- DAISIE_ML_CS(
-    datalist = New_Caledonia,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][3],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5]),
-    idparsopt = 1:5,
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = 1)
-
-  New_Caledonia_cr_dd[[i]] <- DAISIE_ML_CS(
-    datalist = New_Caledonia,
-    initparsopt = c(New_Caledonia_cr_dd_init[[i]]$lambda_c,
-                    New_Caledonia_cr_dd_init[[i]]$mu,
-                    New_Caledonia_cr_dd_init[[i]]$K,
-                    New_Caledonia_cr_dd_init[[i]]$gamma,
-                    New_Caledonia_cr_dd_init[[i]]$lambda_a),
-    idparsopt = 1:5,
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = 1)
-}
-
-#Extract global optima
-loglik <- sapply(New_Caledonia_cr_dd, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-New_Caledonia_cr_dd_max_loglik <- New_Caledonia_cr_dd[[max_loglik]]
-
-#Calculate BIC
-New_Caledonia_cr_dd_BIC <-
-  5 * (log(1000) + log(2 * pi)) - 2 * New_Caledonia_cr_dd[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "New_Caledonia",
-                               "New_Caledonia_cr_dd.RData")
-New_Caledonia_cr_dd_results <- list(
-  New_Caledonia_cr_dd_max_loglik = New_Caledonia_cr_dd_max_loglik,
-  New_Caledonia_cr_dd_BIC = New_Caledonia_cr_dd_BIC)
-
-save(New_Caledonia_cr_dd_results,
-     file = results_file_path)
-
+relaxedDAISIE::archipelago_analysis(data = New_Caledonia,
+                                    archipelago = "New_Caledonia",
+                                    model = "cr_dd",
+                                    idparsopt = 1:5,
+                                    parsfix = NULL,
+                                    idparsfix = NULL,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
