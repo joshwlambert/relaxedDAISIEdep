569a3e35ffc1fcedf73ca604867e8962654fc026
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-27 16:05:50 +0200

    updated format of New_Zealand_rr_mu_dd script

--- scripts/New_Zealand_models/New_Zealand_rr_mu_dd.R
+++ scripts/New_Zealand_models/New_Zealand_rr_mu_dd.R
@@ -1,72 +1,14 @@
-# New Zealand relaxed rate extinction diversity dependent model
-
-library(DAISIE)
+args <- commandArgs(TRUE)
 
 data(New_Zealand)
 
-init_param_set <- list()
-set.seed(1)
-for (i in 1:10) {
-  init_param_set[[i]] <- runif(2, 0.1, 2)
-  init_param_set[[i]][3] <- runif(1, 30, 100)
-  init_param_set[[i]][4] <- runif(1, 0.001, 0.01)
-  init_param_set[[i]][5] <- runif(1, 0.1, 2)
-  init_param_set[[i]][6] <- runif(1, 0.1, 1)
-}
-
-New_Zealand_rr_mu_dd_init <- list()
-New_Zealand_rr_mu_dd <- list()
-
-for (i in seq_along(init_param_set)) {
-  New_Zealand_rr_mu_dd_init[[i]] <- DAISIE_ML_CS(
-    datalist = New_Zealand,
-    initparsopt = c(init_param_set[[i]][1],
-                    init_param_set[[i]][2],
-                    init_param_set[[i]][3],
-                    init_param_set[[i]][4],
-                    init_param_set[[i]][5],
-                    init_param_set[[i]][6]),
-    idparsopt = c(1, 2, 3, 4, 5, 6),
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "extinction"))
-
-  New_Zealand_rr_mu_dd[[i]] <- DAISIE_ML_CS(
-    datalist = New_Zealand,
-    initparsopt = c(New_Zealand_rr_mu_dd_init[[i]]$lambda_c,
-                    New_Zealand_rr_mu_dd_init[[i]]$mu,
-                    New_Zealand_rr_mu_dd_init[[i]]$K,
-                    New_Zealand_rr_mu_dd_init[[i]]$gamma,
-                    New_Zealand_rr_mu_dd_init[[i]]$lambda_a,
-                    New_Zealand_rr_mu_dd_init[[i]]$sd),
-    idparsopt = c(1, 2, 3, 4, 5, 6),
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    optimmethod = "simplex",
-    CS_version = DAISIE::create_CS_version(model = 2,
-                                           relaxed_par = "extinction"))
-}
-
-#Extract global optima
-loglik <- sapply(New_Zealand_rr_mu_dd, '[[', 6)
-max_loglik <- which(max(loglik) == loglik)
-New_Zealand_rr_mu_dd_max_loglik <-
-  New_Zealand_rr_mu_dd[[max_loglik]]
-
-#Calculate BIC
-New_Zealand_rr_mu_dd_BIC <-
-  5 * (log(1000) + log(2 * pi)) - 2 * New_Zealand_rr_mu_dd[[max_loglik]]$loglik
-
-#Save results
-results_file_path <- file.path(getwd(), "results", "New_Zealand",
-                               "New_Zealand_rr_mu_dd.RData")
-New_Zealand_rr_mu_dd_results <- list(
-  New_Zealand_rr_mu_dd_max_loglik = New_Zealand_rr_mu_dd_max_loglik,
-  New_Zealand_rr_mu_dd_BIC = New_Zealand_rr_mu_dd_BIC)
-
-save(New_Zealand_rr_mu_dd_results,
-     file = results_file_path)
+relaxedDAISIE::archipelago_analysis(data = New_Zealand,
+                                    archipelago = "New_Zealand",
+                                    model = "rr_mu_dd",
+                                    idparsopt = 1:6,
+                                    parsfix = NULL,
+                                    idparsfix = NULL,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "extinction")
