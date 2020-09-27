ff1051ab1c05c96dc9ed2d7668c3ba387361a5e1
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-27 17:10:13 +0200

    added Palau_rr_k_no_lambda_c script

---
+++ scripts/Palau_models/Palau_rr_k_no_lambda_c.R
@@ -0,0 +1,14 @@
+args <- commandArgs(TRUE)
+
+data(Palau)
+
+relaxedDAISIE::archipelago_analysis(data = Palau,
+                                    archipelago = "Palau",
+                                    model = "rr_k_no_lambda_c",
+                                    idparsopt = 2:6,
+                                    parsfix = 0,
+                                    idparsfix = 1,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "carrying_capacity")
