76bb93abd44fe8c20a7698dacc3da59e4c3cd1b6
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-28 11:12:00 +0200

    added Samoa_rr_mu_dd_no_lambda_c script

---
+++ scripts/Samoa_models/Samoa_rr_mu_dd_no_lambda_c.R
@@ -0,0 +1,14 @@
+args <- commandArgs(TRUE)
+
+data(Samoa)
+
+relaxedDAISIE::archipelago_analysis(data = Samoa,
+                                    archipelago = "Samoa",
+                                    model = "rr_mu_dd_no_lambda_c",
+                                    idparsopt = 2:6,
+                                    parsfix = 0,
+                                    idparsfix = 1,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "extinction")
