7979644502851d186a8a5312686eb4f308ffa37d
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-29 23:05:18 +0200

    added Seychelles_Inner_rr_mu_dd_no_lambda_c script

---
+++ scripts/Seychelles_Inner_models/Seychelles_Inner_rr_mu_dd_no_lambda_c.R
@@ -0,0 +1,14 @@
+args <- commandArgs(TRUE)
+
+data(Seychelles_Inner)
+
+relaxedDAISIE::archipelago_analysis(data = Seychelles_Inner,
+                                    archipelago = "Seychelles_Inner",
+                                    model = "rr_mu_dd_no_lambda_c",
+                                    idparsopt = 2:6,
+                                    parsfix = 0,
+                                    idparsfix = 1,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "extinction")
