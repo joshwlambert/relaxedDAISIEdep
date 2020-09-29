58c8b871c56ac2fcffd6ebd38d3fea6fe8854a4c
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-29 23:31:47 +0200

    added Mauritius_Island_cr_dd_no_lambda_c script

---
+++ scripts/Mauritius_Island_models/Mauritius_Island_cr_dd_no_lambda_c.R
@@ -0,0 +1,13 @@
+args <- commandArgs(TRUE)
+
+data(Mauritius_Island)
+
+relaxedDAISIE::archipelago_analysis(data = Mauritius_Island,
+                                    archipelago = "Mauritius_Island",
+                                    model = "cr_dd_no_lambda_c",
+                                    idparsopt = 2:5,
+                                    parsfix = 0,
+                                    idparsfix = 1,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
