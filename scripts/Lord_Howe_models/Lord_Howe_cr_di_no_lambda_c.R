9982d3fa52ead4967136f3b726e62a82fdb22cb3
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-29 23:31:12 +0200

    added Lord_Howe_cr_di_no_lambda_C script

---
+++ scripts/Lord_Howe_models/Lord_Howe_cr_di_no_lambda_c.R
@@ -0,0 +1,13 @@
+args <- commandArgs(TRUE)
+
+data(Lord_Howe)
+
+relaxedDAISIE::archipelago_analysis(data = Lord_Howe,
+                                    archipelago = "Lord_Howe",
+                                    model = "cr_di_no_lambda_c",
+                                    idparsopt = c(2, 4, 5),
+                                    parsfix = c(0, Inf),
+                                    idparsfix = c(1, 3),
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = FALSE)
