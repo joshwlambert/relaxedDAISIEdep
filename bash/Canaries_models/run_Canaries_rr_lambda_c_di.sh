4160df059b1ef8574b37f2e5214bb2f80345212e
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-30 23:16:04 +0200

    added run_Canaries_rr_lambda_c_di bash script

---
+++ bash/Canaries_models/run_Canaries_rr_lambda_c_di.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Canaries
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Canaries_rr_lambda_c_di%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Canaries_models/Canaries_rr_lambda_c_di.R ${SLURM_ARRAY_TASK_ID}
