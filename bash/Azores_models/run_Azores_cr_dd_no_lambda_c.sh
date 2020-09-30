d95313c5f9377e623faf8e39f308a7513c671e99
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-30 23:11:22 +0200

    added run_Azores_cr_dd_no_lambda_c bash script

---
+++ bash/Azores_models/run_Azores_cr_dd_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Azores
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Azores_cr_dd_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Azores_models/Azores_cr_dd_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
