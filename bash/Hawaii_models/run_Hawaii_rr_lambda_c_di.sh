41733631422c9eb48d2484945135f99ee47bb317
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-01 17:39:11 +0200

    added run_Hawaii_rr_lambda_c_di bash script

---
+++ bash/Hawaii_models/run_Hawaii_rr_lambda_c_di.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Hawaii
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Hawaii_rr_lambda_c_di%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Hawaii_models/Hawaii_rr_lambda_c_di.R ${SLURM_ARRAY_TASK_ID}
