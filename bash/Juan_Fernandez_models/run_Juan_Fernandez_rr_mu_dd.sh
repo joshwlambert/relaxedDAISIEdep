22b75f1167c8547ab4e6e3890375aa5802d9f7c6
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-01 18:28:19 +0200

    added run_Juan_Fernandez_rr_mu_dd bash script

---
+++ bash/Juan_Fernandez_models/run_Juan_Fernandez_rr_mu_dd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Juan_Fernandez
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Juan_Fernandez_rr_mu_dd%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Juan_Fernandez_models/Juan_Fernandez_rr_mu_dd.R ${SLURM_ARRAY_TASK_ID}
