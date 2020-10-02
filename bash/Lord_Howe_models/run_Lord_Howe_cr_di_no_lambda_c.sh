732eeee39434b474b7be5de23cf8f8a2cec5336d
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-02 12:43:15 +0200

    added Lord_Howe_cr_di_no_lambda_c bash script

---
+++ bash/Lord_Howe_models/run_Lord_Howe_cr_di_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Lord_Howe
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Lord_Howe_cr_di_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Lord_Howe_models/Lord_Howe_cr_di_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
