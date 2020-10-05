48f33765c76e3d17959b11b01f59a250eab04a7e
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 19:19:43 +0200

    updated format of run_Hawaii_models bash script

--- bash/Hawaii_models/run_Hawaii_models.sh
+++ bash/Hawaii_models/run_Hawaii_models.sh
@@ -3,9 +3,13 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Hawaii_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Hawaii_models.log
+#SBATCH --job-name=Hawaii
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Hawaii_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Hawaii_models.R
+mkdir -p results/Hawaii
+models=(cr_di cr_dd cr_di_no_lambda_a cr_dd_no_lambda_a rr_lambda_c_di \
+        rr_lambda_c_dd rr_mu_di rr_mu_dd rr_k rr_lambda_a_di rr_lambda_a_dd)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Hawaii_models/run_Hawaii_${models[SLURM_ARRAY_TASK_ID]}.sh
