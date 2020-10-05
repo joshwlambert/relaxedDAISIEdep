92cf889f671855e13bae9ce75fb9b6a88accd628
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 14:05:38 +0200

    added run_New_Caledonia_cr_di bash script

---
+++ bash/New_Caledonia_models/run_New_Caledonia_cr_di.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=New_Caledonia
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/New_Caledonia_cr_di%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/New_Caledonia_models/New_Caledonia_cr_di.R ${SLURM_ARRAY_TASK_ID}
