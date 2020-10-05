d2040be5d2fa26a89b63d181656c935c0e336575
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 14:11:57 +0200

    added run_Niue_cr_di_no_lambda_c bash script

---
+++ bash/Niue_models/run_Niue_cr_di_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Niue
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Niue_cr_di_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Niue_models/Niue_cr_di_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
