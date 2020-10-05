b83ea179c15fbebfda62f527c2d6248f528e70b5
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 19:16:53 +0200

    edited install_relaxedDAISIE bash script to make island specific log dirs and install remotes

--- bash/install_relaxedDAISIE.sh
+++ bash/install_relaxedDAISIE.sh
@@ -4,11 +4,18 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=install_relaxedDAISIE
-#SBATCH --output=logs/rr_approx_cr.log
+#SBATCH --output=install_relaxedDAISIE.log
 #SBATCH --mem=1GB
 
-mkdir -p logs
+mkdir -p log/Aldabra_Group log/Azores log/Canaries log/Cape_Verde log/Chatham \
+          log/Comoros log/Galapagos log/Greater_Antilles log/Guadalupe \
+          log/Guam_Marianas log/Hawaii log/Hispaniola log/Juan_Fernandez \
+          log/Lord_Howe log/Madeira log/Marquesas log/Mauritius_Island \
+          log/New_Caledonia log/New_Zealand log/Niue log/Norfolk log/Ogasawara \
+          log/Palau log/Reunion log/Samoa log/SaoTome_Principe \
+          log/Seychelles_Inner log/Socorro
 
 module load R
 
-Rscript -e 'devtools::install.github("joshwlambert/relaxedDAISIE")'
+Rscript -e 'install.packages("remotes")'
+Rscript -e 'remotes::install_github("joshwlambert/relaxedDAISIE")'
