2858c12077206aef0a4b24f3b78ed5db986349a6
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 14:38:14 +0200

    added run_Samoa_rr_mu_dd_no_lambda_c bash script

---
+++ bash/Samoa_models/run_Samoa_rr_mu_dd_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Samoa
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Samoa_rr_mu_dd_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Samoa_models/Samoa_rr_mu_dd_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
