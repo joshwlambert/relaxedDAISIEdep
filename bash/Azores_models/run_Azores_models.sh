5b7dfdb6dda9f308c2675df88eea535c699593f8
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 19:17:27 +0200

    updated format of run_Azores_models bash script

--- bash/Azores_models/run_Azores_models.sh
+++ bash/Azores_models/run_Azores_models.sh
@@ -4,12 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=Azores
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Azores%a.log
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Azores_models%a.log
 #SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-mkdir -p results
 mkdir -p results/Azores
-module load R
-models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c rr_lambda_a_dd_no_lambda_c)
-Rscript /home/p287218/relaxedDAISIE/scripts/Azores_models/Azores_Group_${models[SLURM_ARRAY_TASK_ID]}.R
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Azores_models/run_Azores_${models[SLURM_ARRAY_TASK_ID]}.sh
