9bd7f1975b3fdf8b7d268bb70d0c7211d35a9dee
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 19:19:11 +0200

    updated format of run_Greater_Antilles_models bash script

--- bash/Greater_Antilles_models/run_Greater_Antilles_models.sh
+++ bash/Greater_Antilles_models/run_Greater_Antilles_models.sh
@@ -3,9 +3,13 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Greater_Antilles_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Greater_Antilles_models.log
+#SBATCH --job-name=Greater_Antilles
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Greater_Antilles_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Greater_Antilles_models.R
+mkdir -p results/Comoros
+models=(cr_di cr_dd rr_lambda_c_di rr_lambda_c_dd rr_mu_di rr_mu_dd rr_k \
+        rr_lambda_a_di rr_lambda_a_dd)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Greater_Antilles_models/run_Greater_Antilles_${models[SLURM_ARRAY_TASK_ID]}.sh
