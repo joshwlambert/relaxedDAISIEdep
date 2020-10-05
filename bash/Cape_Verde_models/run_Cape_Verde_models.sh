c57c971b4aea1a53bfdfb4705aa8f062eeebd6f4
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 19:18:02 +0200

    updated format of run_Cape_Verde_models bash script

--- bash/Cape_Verde_models/run_Cape_Verde_models.sh
+++ bash/Cape_Verde_models/run_Cape_Verde_models.sh
@@ -3,9 +3,14 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Cape_Verde_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Cape_Verde_models.log
+#SBATCH --job-name=Cape_Verde
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Cape_Verde_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Cape_Verde_models.R
+mkdir -p results/Cape_Verde
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Cape_Verde_models/run_Cape_Verde_${models[SLURM_ARRAY_TASK_ID]}.sh
