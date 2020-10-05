6cf17949511c2b3a245078219e20be38af9d3d61
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-05 14:48:46 +0200

    added run_Seychelles_Inner_rr_mu_di_no_lambda_c bash script

---
+++ bash/Seychelles_Inner_models/run_Seychelles_Inner_rr_mu_di_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Seychelles_Inner
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Seychelles_Inner_rr_mu_di_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=5GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Seychelles_Inner_models/Seychelles_Inner_rr_mu_di_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
