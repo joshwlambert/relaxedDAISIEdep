ee3b0ae224792e606190509d8d4a850115419163
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-06 11:30:52 +0200

    updated format of run_New_Zealand_models bash script

--- bash/New_Zealand_models/run_New_Zealand_models.sh
+++ bash/New_Zealand_models/run_New_Zealand_models.sh
@@ -3,9 +3,13 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=New_Zealand_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/New_Zealand_models.log
+#SBATCH --job-name=New_Zealand
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/New_Zealand_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/New_Zealand_models.R
+mkdir -p results/New_Zealand
+models=(cr_di cr_dd rr_lambda_c_di rr_lambda_c_dd rr_mu_di rr_mu_dd rr_k \
+        rr_lambda_a_di rr_lambda_a_dd)
+
+sbatch /home/p287218/relaxedDAISIE/bash/New_Zealand_models/run_New_Zealand_${models[SLURM_ARRAY_TASK_ID]}.sh
