7b41c148356c6bdfe91850d7e315ab9eb3af0a36
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-06 11:33:04 +0200

    updated format of run_Seychelles_Inner_models bash script

--- bash/Seychelles_Inner_models/run_Seychelles_Inner_models.sh
+++ bash/Seychelles_Inner_models/run_Seychelles_Inner_models.sh
@@ -3,9 +3,14 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Seychelles_Inner_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Seychelles_Inner_models.log
+#SBATCH --job-name=Seychelles_Inner
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Seychelles_Inner_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Seychelles_Inner_models.R
+mkdir -p results/Seychelles_Inner
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Seychelles_Inner_models/run_Seychelles_Inner_${models[SLURM_ARRAY_TASK_ID]}.sh
