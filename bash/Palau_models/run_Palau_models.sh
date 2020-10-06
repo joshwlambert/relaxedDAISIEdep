17ebd420f2a8aa1b84e89ce9bfbf1e3668179eeb
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-06 11:31:54 +0200

    updated format of run_Palau_models bash script

--- bash/Palau_models/run_Palau_models.sh
+++ bash/Palau_models/run_Palau_models.sh
@@ -3,9 +3,14 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Palau_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Palau_models.log
+#SBATCH --job-name=Palau
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Palau_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Palau_models.R
+mkdir -p results/Palau
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Palau_models/run_Palau_${models[SLURM_ARRAY_TASK_ID]}.sh
