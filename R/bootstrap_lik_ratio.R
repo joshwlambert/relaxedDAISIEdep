7c63f4d53f416b2cdf3e7453617205af548eb1fd
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-06 21:53:33 +0200

    comment out code to be worked on later

--- R/bootstrap_lik_ratio.R
+++ R/bootstrap_lik_ratio.R
@@ -171,17 +171,17 @@ bootstrap_lik_ratio <- function(
               ml_2_model_1 = ml_1_model_1,
               ml_1_model_2 = ml_1_model_2,
               ml_2_model_2 = ml_2_model_2))
-
-  lik_ratio_0 <- model_1$loglik - model_2$loglik
-  lik_ratio <- boostrap_ouput$ml_1$loglik - bootstrap_output$ml_2$loglik
-
-
-  r_model_1 <- length(which(lik_ratio > lik_ratio_0))
-  p_value <- (r_model_1 + 1)/(length(lik_ratio) + 1)
-  lik_ratio_alpha <- 100 * (1 - 0.05)
-
-  lik_ratio <- boostrap_output$ml_2$loglik - bootstrap_output$ml_1$loglik
-
-  r_model_2 <- length(which(lik_ratio > lik_ratio_0))
-  power <- r_model_2 / (length(lik_ratio) + 1)
+#
+#   lik_ratio_0 <- model_1$loglik - model_2$loglik
+#   lik_ratio <- boostrap_ouput$ml_1$loglik - bootstrap_output$ml_2$loglik
+#
+#
+#   r_model_1 <- length(which(lik_ratio > lik_ratio_0))
+#   p_value <- (r_model_1 + 1)/(length(lik_ratio) + 1)
+#   lik_ratio_alpha <- 100 * (1 - 0.05)
+#
+#   lik_ratio <- boostrap_output$ml_2$loglik - bootstrap_output$ml_1$loglik
+#
+#   r_model_2 <- length(which(lik_ratio > lik_ratio_0))
+#   power <- r_model_2 / (length(lik_ratio) + 1)
 }
