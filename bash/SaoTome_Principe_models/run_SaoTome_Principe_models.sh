e2d645e8f549613d72f750febabc6313b8cd0beb
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-06 11:32:48 +0200

    updated format of run_SaoTome_Principe_models bash script

--- bash/SaoTome_Principe_models/run_SaoTome_Principe_models.sh
+++ bash/SaoTome_Principe_models/run_SaoTome_Principe_models.sh
@@ -3,9 +3,13 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=SaoTome_Principe_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/SaoTome_Principe_models.log
+#SBATCH --job-name=SaoTome_Principe
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/SaoTome_Principe_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/SaoTome_Principe_models.R
+mkdir -p results/SaoTome_Principe
+models=(cr_di cr_dd rr_lambda_c_di rr_lambda_c_dd rr_mu_di rr_mu_dd rr_k \
+        rr_lambda_a_di rr_lambda_a_dd)
+
+sbatch /home/p287218/relaxedDAISIE/bash/SaoTome_Principe_models/run_SaoTome_Principe_${models[SLURM_ARRAY_TASK_ID]}.sh
