6759999e899d8b1954fcf4f03760ed0033c12c59
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-06 11:29:00 +0200

    updated format of run_Hispaniola_models bash script

--- bash/Hispaniola_models/run_Hispaniola_models.sh
+++ bash/Hispaniola_models/run_Hispaniola_models.sh
@@ -3,9 +3,14 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=Hispaniola_models
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Hispaniola_models.log
+#SBATCH --job-name=Hispaniola
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Hispaniola_models%a.log
+#SBATCH --array=0-8
 #SBATCH --mem=5GB
 
-module load R
-Rscript /home/p287218/relaxedDAISIE/scripts/Hispaniola_models.R
+mkdir -p results/Hispaniola
+models=(cr_di cr_dd cr_di_no_lambda_a cr_dd_no_lambda_a rr_lambda_c_no_lambda_a \
+        rr_lambda_c_no_lambda_a rr_mu_di_no_lambda_a rr_mu_dd_no_lambda_a \
+        rr_k_no_lambda_a)
+
+sbatch /home/p287218/relaxedDAISIE/bash/Hispaniola_models/run_Hispaniola_${models[SLURM_ARRAY_TASK_ID]}.sh
