62aa233c66a84d2ffe2918b81ee2776e78bc1feb
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-12 17:30:15 +0200

    fixed broken test in bootstrap_lik_ratio tests

--- tests/testthat/test-bootstrap_lik_ratio.R
+++ tests/testthat/test-bootstrap_lik_ratio.R
@@ -1,7 +1,7 @@
 test_that("bootstrap produces the correct output", {
   if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
     data(Aldabra_Group)
-    output <- bootstrap_lik_ratio(
+    output <- relaxedDAISIE::bootstrap_lik_ratio(
       data = Aldabra_Group,
       archipelago = "Aldabra_Group",
       model_1 = data.frame(lambda_c = 0.8135786,
@@ -26,47 +26,49 @@ test_that("bootstrap produces the correct output", {
       parsfix_2 = NULL,
       idparsfix_1 = NULL,
       idparsfix_2 = NULL,
+      ddmodel_1 = 11,
+      ddmodel_2 = 11,
       relaxed_model_1 = FALSE,
       relaxed_model_2 = FALSE,
       relaxed_par_1 = NULL,
       relaxed_par_2 = NULL,
       seed = 1)
 
-    expect_output <- list(
+    expected_output <- list(
       ml_1_model_1 = data.frame(
-        lambda_c = 4.78895599143e-07,
-        mu = 3.25442916604e-07,
-        K = 0.285539775068,
-        gamma = 0.0119247136336,
-        lambda_a = 0.925702053638,
-        loglik = -70.3043959543,
+        lambda_c = 1.571382865,
+        mu = 4.73965052627e-11,
+        K = 0.797554886102,
+        gamma = 0.0120092155254,
+        lambda_a = 0.925702500114,
+        loglik = -70.230659207,
         df = 5,
         conv = 0),
       ml_2_model_1 = data.frame(
-        lambda_c = 0.8135786,
-        mu = 0.6530319,
-        K = 0.5210479,
-        gamma = 0.01645943,
-        lambda_a = 1.01026,
-        loglik = -73.36238,
+        lambda_c = 1.571382865,
+        mu = 4.73965052627e-11,
+        K = 0.797554886102,
+        gamma = 0.0120092155254,
+        lambda_a = 0.925702500114,
+        loglik = -70.230659207,
         df = 5,
         conv = 0),
       ml_1_model_2 = data.frame(
-        lambda_c = 4.78895599143e-07,
-        mu = 3.25442916604e-07,
-        K = 0.285539775068,
-        gamma = 0.0119247136336,
-        lambda_a = 0.925702053638,
-        loglik = -70.3043959543,
+        lambda_c = 1.571382865,
+        mu = 4.73965052627e-11,
+        K = 0.797554886102,
+        gamma = 0.0120092155254,
+        lambda_a = 0.925702500114,
+        loglik = -70.230659207,
         df = 5,
         conv = 0),
       ml_2_model_2 = data.frame(
-        lambda_c = 4.78895599143e-07,
-        mu = 3.25442916604e-07,
-        K = 0.285539775068,
-        gamma = 0.0119247136336,
-        lambda_a = 0.925702053638,
-        loglik = -70.3043959543,
+        lambda_c = 0.80670520808,
+        mu = 1.82754079009,
+        K = 0.352536741622,
+        gamma = 0.0160496378021,
+        lambda_a = 2.43890296807,
+        loglik = -49.2578885912,
         df = 5,
         conv = 0))
 
