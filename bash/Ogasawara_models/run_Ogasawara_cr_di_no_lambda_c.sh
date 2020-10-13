f6cfc85d8f44eb82a4dafbe480a00d64815149fd
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-13 19:18:14 +0200

    changed log file address for Ogasawara_models

--- bash/Ogasawara_models/run_Ogasawara_cr_di_no_lambda_c.sh
+++ bash/Ogasawara_models/run_Ogasawara_cr_di_no_lambda_c.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=Ogasawara
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Ogasawara_cr_di_no_lambda_c%a.log
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Ogasawara/Ogasawara_cr_di_no_lambda_c%a.log
 #SBATCH --array=1-10
 #SBATCH --mem=5GB
 
