070ea4726a440904f3534021b43082ef9807fb80
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-13 13:10:22 +0200

    changed log file address for Greater_Antilles_models

--- bash/Greater_Antilles_models/run_Greater_Antilles_rr_lambda_a_dd.sh
+++ bash/Greater_Antilles_models/run_Greater_Antilles_rr_lambda_a_dd.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=Greater_Antilles
-#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Greater_Antilles_rr_lambda_a_dd%a.log
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Greater_Antilles/Greater_Antilles_rr_lambda_a_dd%a.log
 #SBATCH --array=1-10
 #SBATCH --mem=5GB
 
