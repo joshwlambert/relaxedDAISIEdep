59c395f298e24f7fd33e1ae57607c715363d800f
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-14 22:18:21 +0200

    wrote tests for calc_p_value

---
+++ tests/testthat/test-calc_p_value.R
@@ -0,0 +1,23 @@
+test_that("calc_p_value produces correct output", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+    set.seed(1)
+    output <- calc_p_value(lik_ratio_0 = 10,
+                           lik_ratio_model_1 = runif(1000, 0, 100))
+    expected_output <- 0.886113886114
+    expect_equal(output, expected_output)
+  } else{
+    skip("Run only on TRAVIS and AppVeyor")
+  }
+})
+
+test_that("calc_p_value produces error", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+    expect_error(calc_p_value(lik_ratio_0 = "string",
+                              lik_ratio_model_1 = runif(1000, 0, 100)))
+    expect_error(calc_p_value(lik_ratio_0 = 10,
+                              lik_ratio_model_1 = "string"))
+  } else{
+    skip("Run only on TRAVIS and AppVeyor")
+  }
+})
+
