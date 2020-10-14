8333e627d22fa80561b8937ee8daabfde1fdb493
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-14 18:30:44 +0200

    updated Aldabra_Group_bootstrap script

--- scripts/Aldabra_Group_models/Aldabra_Group_boostrap.R
+++ scripts/Aldabra_Group_models/Aldabra_Group_boostrap.R
@@ -2,15 +2,4 @@ args <- commandArgs(TRUE)
 
 data(Aldabra_Group)
 
-load(.../model_1_output)
-load(.../model_2_output)
-
-relaxedDAISIE::bootstrap(data = Aldabra_Group,
-                         archipelago = "Aldabra_Group",
-                         model_1 = "cr_di",
-                         model_2 = "cr_dd",
-                         model_1_output = ,
-                         model_2_output = ,
-                         relaxed_model_1 = FALSE,
-                         relaxed_model_2 = FALSE,
-                         seed = args[1])
+relaxedDAISIE::bootstrap_lik_ratio()
