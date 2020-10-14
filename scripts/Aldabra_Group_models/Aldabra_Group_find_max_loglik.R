56fc96c77fc39b53de56d9cf9436c0b677a707e3
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-14 12:53:26 +0200

    added Aldabra_Group_find_max_loglik script

---
+++ scripts/Aldabra_Group_models/Aldabra_Group_find_max_loglik.R
@@ -0,0 +1,15 @@
+max_loglik_cr_di <- find_max_loglik(
+  archipelago = "Aldabra_Group",
+  model = "cr_di")
+
+max_loglik_cr_dd <- find_max_loglik(
+  archipelago = "Aldabra_Group",
+  model = "cr_dd")
+
+max_loglik_cr_di_no_lambda_c <- find_max_loglik(
+  archipelago = "Aldabra_Group",
+  model = "cr_di_no_lambda_c")
+
+max_loglik_cr_dd_no_lambda_c <- find_max_loglik(
+  archipelago = "Aldabra_Group",
+  model = "cr_dd_no_lambda_c")
