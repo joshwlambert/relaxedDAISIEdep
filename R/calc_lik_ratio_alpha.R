298cbd3e8b46e0e97301b345bb36f59783f3dbe4
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-14 22:17:33 +0200

    added asserts to calc_lik_ratio_alpha

--- R/calc_lik_ratio_alpha.R
+++ R/calc_lik_ratio_alpha.R
@@ -9,6 +9,7 @@
 #' @examples output <- calc_lik_ratio_alpha(
 #' lik_ratio_model_1 = runif(1000, 0, 100))
 calc_lik_ratio_alpha <- function(lik_ratio_model_1) {
+  testit::assert(is.numeric(lik_ratio_model_1))
   lik_ratio_alpha <- as.numeric(stats::quantile(lik_ratio_model_1,
                   1 - 0.05,
                   type = 4))
