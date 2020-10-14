b27048ffe2eec8dc5d523e6c086872403fbbcea0
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-14 22:23:47 +0200

    renamed find_max_loglik tests to calc_max_loglik

---
+++ tests/testthat/test-calc_max_loglik.R
@@ -0,0 +1,14 @@
+test_that("calc_max_loglik produces correct output", {
+  output <- find_max_loglik(archipelago = "Aldabra_Group",
+                            model = "cr_dd")
+  expected_output <- list(ml = data.frame(lambda_c = 2.95333497114e-11,
+                                          mu = 0.652208416987,
+                                          K = 0.780567632535,
+                                          gamma = 0.0163642212586,
+                                          lambda_a = 1.01019154261,
+                                          loglik = -73.3548314268,
+                                          df = 5,
+                                          conv = 0),
+                          bic = 190.437824581)
+  expect_equal(output, expected_output)
+})
--- tests/testthat/test-find_max_loglik.R
+++
@@ -1,4 +0,0 @@
-test_that("multiplication works", {
-  skip("WIP")
-  output <- find_max_loglik(archipelago = "Aldabra_Group")
-})
