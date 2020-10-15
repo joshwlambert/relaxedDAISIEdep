ada1b3d533aec6c0d31736a5d8443f62f4f66978
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 12:40:03 +0200

    added run_Lord_Howe_bootstrap bash script

---
+++ bash/Lord_Howe_models/run_Lord_Howe_bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Lord_Howe
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Lord_Howe/Lord_Howe_bootstrap%a.log
+#SBATCH --array=1-1000
+#SBATCH --mem=1GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Lord_Howe_models/Lord_Howe_bootstrap.R ${SLURM_ARRAY_TASK_ID}
