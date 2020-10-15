ecb9b4f1555b8fc5c1a8b868895a5fa6407515f2
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 16:06:44 +0200

    changed Comoros_models to 1GB

--- bash/Comoros_models/run_Comoros_cr_dd.sh
+++ bash/Comoros_models/run_Comoros_cr_dd.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=Comoros
 #SBATCH --output=/home/p287218/relaxedDAISIE/logs/Comoros/Comoros_cr_dd%a.log
 #SBATCH --array=1-10
-#SBATCH --mem=5GB
+#SBATCH --mem=1GB
 
 module load R
 Rscript /home/p287218/relaxedDAISIE/scripts/Comoros_models/Comoros_cr_dd.R ${SLURM_ARRAY_TASK_ID}
--- bash/Comoros_models/run_Comoros_rr_mu_di.sh
+++ bash/Comoros_models/run_Comoros_rr_mu_di.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=Comoros
 #SBATCH --output=/home/p287218/relaxedDAISIE/logs/Comoros/Comoros_rr_mu_di%a.log
 #SBATCH --array=1-10
-#SBATCH --mem=5GB
+#SBATCH --mem=1GB
 
 module load R
 Rscript /home/p287218/relaxedDAISIE/scripts/Comoros_models/Comoros_rr_mu_di.R ${SLURM_ARRAY_TASK_ID}
