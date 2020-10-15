0fd572347d5d25b5c194e2daa1221021d02c6636
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 11:01:03 +0200

    fixed reference values for calc_p_value

--- tests/testthat/test-calc_p_value.R
+++ tests/testthat/test-calc_p_value.R
@@ -3,7 +3,7 @@ test_that("calc_p_value produces correct output", {
     set.seed(1)
     output <- calc_p_value(lik_ratio_0 = 10,
                            lik_ratio_model_1 = runif(1000, 0, 100))
-    expected_output <- 0.886113886114
+    expected_output <- 0.904095904096
     expect_equal(output, expected_output)
   } else{
     skip("Run only on TRAVIS and AppVeyor")
