62634b013c41c24c7457c5684ade931bde4ba376
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 12:38:17 +0200

    added run_Greater_Antilles_bootstrap bash script

---
+++ bash/Greater_Antilles_models/run_Greater_Antilles_bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Greater_Antilles
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Greater_Antilles/Greater_Antilles_bootstrap%a.log
+#SBATCH --array=1-1000
+#SBATCH --mem=1GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Greate_Antilles_models/Greater_Antilles_bootstrap.R ${SLURM_ARRAY_TASK_ID}
