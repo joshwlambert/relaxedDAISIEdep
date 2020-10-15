2d9af61d187df13e7a132d879b6993e4df40087c
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 12:49:04 +0200

    added run_Samoa_bootstrap bash script

---
+++ bash/Samoa_models/run_Samoa_bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Samoa
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Samoa/Samoa_bootstrap%a.log
+#SBATCH --array=1-1000
+#SBATCH --mem=1GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Samoa_models/Samoa_bootstrap.R ${SLURM_ARRAY_TASK_ID}
