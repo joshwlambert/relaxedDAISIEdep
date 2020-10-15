285cb2bca954384653411d17000d8e17135698e2
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 11:00:43 +0200

    fixed calc_max_loglik tests

--- tests/testthat/test-calc_max_loglik.R
+++ tests/testthat/test-calc_max_loglik.R
@@ -1,5 +1,5 @@
 test_that("calc_max_loglik produces correct output", {
-  output <- find_max_loglik(archipelago = "Aldabra_Group",
+  output <- calc_max_loglik(archipelago = "Aldabra_Group",
                             model = "cr_dd")
   expected_output <- list(ml = data.frame(lambda_c = 2.95333497114e-11,
                                           mu = 0.652208416987,
