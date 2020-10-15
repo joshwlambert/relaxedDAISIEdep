6e6f49ddce24da3afcd6458db9dd6c48cbb8482a
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 12:39:06 +0200

    added run_Hispaniola_bootstrap bash script

---
+++ bash/Hispaniola_models/run_Hispaniola_bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Hispaniola
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Hispaniola/Hispaniola_bootstrap%a.log
+#SBATCH --array=1-1000
+#SBATCH --mem=1GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Hispaniola_models/Hispaniola_bootstrap.R ${SLURM_ARRAY_TASK_ID}
