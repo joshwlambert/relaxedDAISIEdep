936b4f610edcad3136adf34b89135c1ed1f01bdd
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 12:37:40 +0200

    added run_Comoros_bootstrap bash script

---
+++ bash/Comoros_models/run_Comoros_bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Comoros
+#SBATCH --output=/home/p287218/relaxedDAISIE/logs/Comoros/Comoros_bootstrap%a.log
+#SBATCH --array=1-1000
+#SBATCH --mem=1GB
+
+module load R
+Rscript /home/p287218/relaxedDAISIE/scripts/Comoros_models/Comoros_bootstrap.R ${SLURM_ARRAY_TASK_ID}
