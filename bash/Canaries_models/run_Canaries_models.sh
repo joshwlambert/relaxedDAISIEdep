5ec4e3499d80bd376979a31e409cffbd436676e3
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-15 15:52:00 +0200

    changed Canaries_models to 1GB

--- bash/Canaries_models/run_Canaries_models.sh
+++ bash/Canaries_models/run_Canaries_models.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=Canaries
 #SBATCH --output=/home/p287218/relaxedDAISIE/logs/Canaries/Canaries_models%a.log
 #SBATCH --array=0-8
-#SBATCH --mem=5GB
+#SBATCH --mem=1GB
 
 mkdir -p results/Canaries
 models=(cr_di cr_dd rr_lambda_c_di rr_lambda_c_dd rr_mu_di rr_mu_dd rr_k \
--- bash/Canaries_models/run_Canaries_rr_k.sh
+++ bash/Canaries_models/run_Canaries_rr_k.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=Canaries
 #SBATCH --output=/home/p287218/relaxedDAISIE/logs/Canaries/Canaries_rr_k%a.log
 #SBATCH --array=1-10
-#SBATCH --mem=5GB
+#SBATCH --mem=1GB
 
 module load R
 Rscript /home/p287218/relaxedDAISIE/scripts/Canaries_models/Canaries_rr_k.R ${SLURM_ARRAY_TASK_ID}
