2ca871da0822b08426467aa4f333540dcec0b06b
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-16 15:40:01 +0200

    added test for saving in bootstrap_lik_ratio

--- tests/testthat/test-bootstrap_lik_ratio.R
+++ tests/testthat/test-bootstrap_lik_ratio.R
@@ -1,4 +1,4 @@
-test_that("bootstrap produces the correct output", {
+test_that("bootstrap_lik_ratio produces the correct output", {
   if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
     data(Aldabra_Group)
     output <- relaxedDAISIE::bootstrap_lik_ratio(
@@ -72,6 +72,54 @@ test_that("bootstrap produces the correct output", {
                                                       conv = 0))
 
     expect_equal(output, expected_output)
+  } else{
+    skip("Run only on TRAVIS and AppVeyor")
+  }
+})
+
+test_that("bootstrap_lik_ratio correctly saves output", {
+  if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
+    data(Aldabra_Group)
+    relaxedDAISIE::bootstrap_lik_ratio(
+      data = Aldabra_Group,
+      archipelago = "Aldabra_Group",
+      model_1 = data.frame(lambda_c = 1.5,
+                           mu = 1,
+                           K = 40,
+                           gamma = 0.01,
+                           lambda_a = 1,
+                           loglik = -75,
+                           df = 5,
+                           conv = 0),
+      model_2 = data.frame(lambda_c = 1,
+                           mu = 0.8,
+                           K = Inf,
+                           gamma = 00.1,
+                           lambda_a = 1,
+                           loglik = -80,
+                           df = 4,
+                           conv = 0),
+      idparsopt_1 = c(1, 2, 3, 4, 5),
+      idparsopt_2 = c(1, 2, 4, 5),
+      parsfix_1 = NULL,
+      parsfix_2 = Inf,
+      idparsfix_1 = NULL,
+      idparsfix_2 = 3,
+      ddmodel_1 = 11,
+      ddmodel_2 = 0,
+      relaxed_model_1 = FALSE,
+      relaxed_model_2 = FALSE,
+      relaxed_par_1 = NULL,
+      relaxed_par_2 = NULL,
+      seed = 1,
+      save_output = TRUE)
+
+    output <- file.exists(paste(
+      "C:/Users/user/Documents/github/relaxedDAISIE/",
+      "data/Aldabra_Group/Aldabra_Group.RData",
+      sep = ""))
+
+    expect_true(output)
   } else{
     skip("Run only on TRAVIS and AppVeyor")
   }
