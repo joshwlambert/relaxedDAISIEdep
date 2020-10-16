3c4ad32b280048f911459de1d7a0acf3538552d8
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-16 22:17:34 +0200

    fixed reference values in calc_max_loglik tests

--- tests/testthat/test-calc_max_loglik.R
+++ tests/testthat/test-calc_max_loglik.R
@@ -1,16 +1,16 @@
 test_that("calc_max_loglik produces correct output", {
   if (Sys.getenv("TRAVIS") != "" || Sys.getenv("APPVEYOR") != "") {
     output <- calc_max_loglik(archipelago = "Aldabra_Group",
-                              model = "cr_dd")
-    expected_output <- list(ml = data.frame(lambda_c = 2.95333497114e-11,
-                                            mu = 0.652208416987,
-                                            K = 0.780567632535,
-                                            gamma = 0.0163642212586,
-                                            lambda_a = 1.01019154261,
-                                            loglik = -73.3548314268,
+                              model = "cr_di")
+    expected_output <- list(list(ml = data.frame(lambda_c = 4.20066537381e-12,
+                                            mu = 0.621368299046,
+                                            K = 46.2764408939,
+                                            gamma = 0.0160573857279,
+                                            lambda_a = 1.00780442154,
+                                            loglik = -73.4101645492,
                                             df = 5,
                                             conv = 0),
-                            bic = 190.437824581)
+                            bic = 190.548490825))
     expect_equal(output, expected_output)
   } else{
     skip("Run only on TRAVIS and AppVeyor")
