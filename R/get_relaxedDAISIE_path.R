02cc18f33e949a575ec37d0c9754b3a03d210279
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-20 12:54:48 +0200

    deleted get_relaxedDAISIE_path

--- R/get_relaxedDAISIE_path.R
+++
@@ -1,8 +0,0 @@
-get_relaxedDAISIE_path <- function(filename, archipelago) {
-  full <- system.file("results", archipelago, filename,
-                      package = "relaxedDAISIE")
-  if (!file.exists(full)) {
-    stop("'filename' must be the name of a file in 'inst/results'")
-  }
-  return(full)
-}
