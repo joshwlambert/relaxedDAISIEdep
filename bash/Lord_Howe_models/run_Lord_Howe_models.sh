a2996cd0a1b401a7c2904b5ff10ff89e88fdf64b
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-25 20:43:55 +0100

    added relaxed-rate models to Lord_Howe bash scripts

--- bash/Lord_Howe_models/run_Lord_Howe_models.sh
+++ bash/Lord_Howe_models/run_Lord_Howe_models.sh
@@ -9,6 +9,8 @@
 #SBATCH --mem=1GB
 
 mkdir -p inst/results/Lord_Howe
-models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c)
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
 
 sbatch /home/p287218/relaxedDAISIE/bash/Lord_Howe_models/run_Lord_Howe_${models[SLURM_ARRAY_TASK_ID]}.sh
