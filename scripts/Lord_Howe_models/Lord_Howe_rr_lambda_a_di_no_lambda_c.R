6207dc2c1251d188d992750590e87a4a5b37d89e
joshwlambert <j.w.l.lambert@rug.nl>
2020-10-25 20:43:36 +0100

    added relaxed-rate Lord_Howe R scripts

---
+++ scripts/Lord_Howe_models/Lord_Howe_rr_lambda_a_di_no_lambda_c.R
@@ -0,0 +1,14 @@
+args <- commandArgs(TRUE)
+
+data(Lord_Howe)
+
+relaxedDAISIE::archipelago_analysis(data = Lord_Howe,
+                                    archipelago = "Lord_Howe",
+                                    model = "rr_lambda_a_di_no_lambda_c",
+                                    idparsopt = c(2, 4, 5, 6),
+                                    parsfix = c(0, Inf),
+                                    idparsfix = c(1, 3),
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "anagenesis")
