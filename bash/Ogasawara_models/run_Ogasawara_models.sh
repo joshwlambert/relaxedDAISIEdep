451a119f6f011be4785c1154ece4f7096de61508
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-06 23:49:57 +0100

    updated run_Ogasawara_models bash script

--- bash/Ogasawara_models/run_Ogasawara_models.sh
+++ bash/Ogasawara_models/run_Ogasawara_models.sh
@@ -5,10 +5,12 @@
 #SBATCH --ntasks=1
 #SBATCH --job-name=Ogasawara
 #SBATCH --output=/data/p287218/relaxedDAISIE/logs/Ogasawara/Ogasawara_models%a.log
-#SBATCH --array=0-3
+#SBATCH --array=0-8
 #SBATCH --mem=1GB
 
 mkdir -p inst/results/Ogasawara
-models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c)
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
 
 sbatch /data/p287218/relaxedDAISIE/bash/Ogasawara_models/run_Ogasawara_${models[SLURM_ARRAY_TASK_ID]}.sh
