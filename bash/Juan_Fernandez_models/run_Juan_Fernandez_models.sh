e04549ffeda3d6628392b78857bdf3e94e1b2e7e
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-06 16:20:08 +0100

    updated run_Juan_Fernandez_models bash script

--- bash/Juan_Fernandez_models/run_Juan_Fernandez_models.sh
+++ bash/Juan_Fernandez_models/run_Juan_Fernandez_models.sh
@@ -5,11 +5,10 @@
 #SBATCH --ntasks=1
 #SBATCH --job-name=Juan_Fernandez
 #SBATCH --output=/data/p287218/relaxedDAISIE/logs/Juan_Fernandez/Juan_Fernandez_models%a.log
-#SBATCH --array=0-8
+#SBATCH --array=0-1
 #SBATCH --mem=1GB
 
 mkdir -p inst/results/Juan_Fernandez
-models=(cr_di cr_dd rr_lambda_c_di rr_lambda_c_dd rr_mu_di rr_mu_dd rr_k \
-        rr_lambda_a_di rr_lambda_a_dd)
+models=(cr_di cr_dd)
 
 sbatch /data/p287218/relaxedDAISIE/bash/Juan_Fernandez_models/run_Juan_Fernandez_${models[SLURM_ARRAY_TASK_ID]}.sh
