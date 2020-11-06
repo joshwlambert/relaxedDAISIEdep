74fc9fdeb5fc525fc0632ab648ad3a98d04167fb
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-06 15:32:26 +0100

    added relaxed-rate script to Socorro

---
+++ scripts/Socorro_models/Socorro_rr_mu_di_no_lambda_c.R
@@ -0,0 +1,14 @@
+args <- commandArgs(TRUE)
+
+data(Socorro)
+
+relaxedDAISIE::archipelago_analysis(data = Socorro,
+                                    archipelago = "Socorro",
+                                    model = "rr_mu_di_no_lambda_c",
+                                    idparsopt = c(2, 4, 5, 6),
+                                    parsfix = c(0, Inf),
+                                    idparsfix = c(1, 3),
+                                    ddmodel = 0,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "extinction")
