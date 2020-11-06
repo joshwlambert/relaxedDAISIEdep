83d9383cd92b4d2a6151f842fcc9445d79963594
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-06 23:50:34 +0100

    updated run_Reunion_models bash script

--- bash/Reunion_models/run_Reunion_models.sh
+++ bash/Reunion_models/run_Reunion_models.sh
@@ -9,6 +9,8 @@
 #SBATCH --mem=1GB
 
 mkdir -p inst/results/Reunion
-models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c)
+models=(cr_di cr_dd cr_di_no_lambda_c cr_dd_no_lambda_c rr_mu_di_no_lambda_c \
+        rr_mu_dd_no_lambda_c rr_k_no_lambda_c rr_lambda_a_di_no_lambda_c \
+        rr_lambda_a_dd_no_lambda_c)
 
 sbatch /data/p287218/relaxedDAISIE/bash/Reunion_models/run_Reunion_${models[SLURM_ARRAY_TASK_ID]}.sh
