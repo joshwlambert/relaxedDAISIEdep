c9a0095e088c6a2106928f57e4da4f33b4a3d004
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-06 23:50:20 +0100

    added Ogasawara relaxed-rate bash scripts

---
+++ bash/Ogasawara_models/run_Ogasawara_rr_lambda_a_di_no_lambda_c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=2-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=Ogasawara
+#SBATCH --output=/data/p287218/relaxedDAISIE/logs/Ogasawara/Ogasawara_rr_lambda_a_di_no_lambda_c%a.log
+#SBATCH --array=1-10
+#SBATCH --mem=1GB
+
+module load R
+Rscript /data/p287218/relaxedDAISIE/scripts/Ogasawara_models/Ogasawara_rr_lambda_a_di_no_lambda_c.R ${SLURM_ARRAY_TASK_ID}
