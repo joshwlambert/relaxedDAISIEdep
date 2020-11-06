a1d5486f6825693613ad9acfbd5f0f18219a7f95
joshwlambert <j.w.l.lambert@rug.nl>
2020-11-06 15:04:43 +0100

    added relaxed-rate scripts to Reunion

---
+++ scripts/Reunion_models/Reunion_rr_mu_dd_no_lambda_c.R
@@ -0,0 +1,14 @@
+args <- commandArgs(TRUE)
+
+data(Reunion)
+
+relaxedDAISIE::archipelago_analysis(data = Reunion,
+                                    archipelago = "Reunion",
+                                    model = "rr_mu_dd_no_lambda_c",
+                                    idparsopt = 2:6,
+                                    parsfix = 0,
+                                    idparsfix = 1,
+                                    ddmodel = 11,
+                                    seed = args[1],
+                                    relaxed_model = TRUE,
+                                    relaxed_par = "extinction")
