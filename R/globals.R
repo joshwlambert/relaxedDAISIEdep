f83f1324ce47b5055c54ac0295a3cb5e0336981e
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-15 15:30:47 +0100

    deleted globals

--- R/globals.R
+++
@@ -1,7 +0,0 @@
-utils::globalVariables(c("archipelagos41", "Azores", "Canaries", "Cape_Verde",
-                         "Chatham", "Galapagos", "Greater_Antilles", "Guadalupe",
-                         "Hawaii", "Juan_Fernandez", "Lord_Howe", "Madeira",
-                         "Guam_Marianas", "Marquesas", "Mauritius_Island",
-                         "New_Caledonia", "Niue", "Norfolk", "Ogasawara",
-                         "Palau", "Reunion", "Samoa", "SaoTome_Principe",
-                         "Socorro"))
