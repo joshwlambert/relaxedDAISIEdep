c579f4462aff4410018440688d1b0357a35934d2
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-31 21:43:55 +0200

    updated documentation

--- R/default_params_doc.R
+++ R/default_params_doc.R
@@ -1,75 +1,9 @@
 #' Document to inherit the function documentation
-#' @param data list of DAISIE data
-#' @param archipelago string with the name of the archipelago
-#' @param model string with the name of the model
-#' @param idparsopt numeric vector with the id of parameters to optimise
-#' @param parsfix numeric vector with the value of the fixed parameters
-#' @param idparsfix numeric vector with the id of parameters to fix
-#' @param ddmodel numeric determining diversity-independence or -dependence
-#' @param seed numeric seed for the simulation
-#' @param relaxed_model boolean defining if the model is relaxed-rate
-#' @param relaxed_par string determining which parameter to relax, only required
-#' if relaxed_model is TRUE
-#' @param save_output boolean determining whether the output is saved or
-#' returned
-#' @param model_1 dataframe containing the maximum likelihood output of a model
-#' from the function \code{archipelago_analysis}
-#' @param model_2 dataframe containing the maximum likelihood output of a model
-#' from the function \code{archipelago_analysis}
-#' @param idparsopt_1 numeric vector with the id of parameters to optimise
-#' @param idparsopt_2 numeric vector with the id of parameters to optimise
-#' @param parsfix_1 numeric vector with the value of the fixed parameters
-#' @param parsfix_2 numeric vector with the value of the fixed parameters
-#' @param idparsfix_1 numeric vector with the id of parameters to fix
-#' @param idparsfix_2 numeric vector with the id of parameters to fix
-#' @param ddmodel_1 numeric determining diversity-dependence in the model
-#' @param ddmodel_2 numeric determining diversity-dependence in the model
-#' @param relaxed_model_1 boolean defining if the model is relaxed-rate
-#' @param relaxed_model_2 boolean defining if the model is relaxed-rate
-#' @param relaxed_par_1 string determining which parameter to relax, only
-#' required if relaxed_model is TRUE
-#' @param relaxed_par_2 string determining which parameter to relax, only
-#' required if relaxed_model is TRUE
-#' @param lik_ratio_0 numeric initial likelihood ratio calculated from the
-#' likelihoods of the models fit to empirical data
-#' @param lik_ratio_model_1 numeric vector of likelihood ratios calculated from
-#' fitting two models to a data set simulated under model 1
-#' @param lik_ratio_model_2 numeric vector of likelihood ratios calculated from
-#' fitting two models to a data set simulated under model 2
-#' @param lik_ratio_alpha The likelihood ratio associated with the significance
-#' of the likelihood ratio test
-#' @param verbose logical determining whether function prints
+#'  @param parameter String determining which parameter is relaxed
+#'  @param clade String either "lineage" or "clade"
 default_params_doc <- function(
-  data,
-  archipelago,
-  model,
-  idparsopt,
-  parsfix,
-  idparsfix,
-  ddmodel,
-  seed,
-  relaxed_model,
-  relaxed_par,
-  save_output,
-  model_1,
-  model_2,
-  idparsopt_1,
-  idparsopt_2,
-  parsfix_1,
-  parsfix_2,
-  idparsfix_1,
-  idparsfix_2,
-  ddmodel_1,
-  ddmodel_2,
-  relaxed_model_1,
-  relaxed_model_2,
-  relaxed_par_1,
-  relaxed_par_2,
-  lik_ratio_0,
-  lik_ratio_model_1,
-  lik_ratio_model_2,
-  lik_ratio_alpha,
-  verbose
+  parameter,
+  clade
 ) {
   # Nothing
 }
